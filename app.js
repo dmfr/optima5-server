d39c6f8761f6525d35b77c7bbeac08a80830b6cc
Damien Mirand <dm@mirabel-sil.com>
2012-04-19 02:20:02 +0200

    JS Documentations for 4.0.7 workarounds

--- app.js
+++ app.js
@@ -14,10 +14,10 @@ Ext.require('Optima5.CoreDesktop.OptimaDesktop');
 var op5desktop, op5session ;
 Ext.onReady(function () {
 	
-	// console.dir( Ext.Loader.history ) ;
-	
 	/*
 	Overrides for fixing clearOnLoad for TreeStore
+	http://www.sencha.com/forum/showthread.php?154059-4.0.7-TreePanel-Error-when-reloading-the-treeStore
+	http://www.sencha.com/forum/showthread.php?154318-url-is-undefined-on-TreeStore
 	*/
 	Ext.override(Ext.data.TreeStore, {
 		load: function(options) {
@@ -52,6 +52,11 @@ Ext.onReady(function () {
 		}
 	});
 
+	/*
+	Problème : erreur au destroy + recréation du FilePanel / BiblePanel
+	"this.tempHidden is undefined" Error Workaround 
+	http://www.sencha.com/forum/showthread.php?160222-quot-this.tempHidden-is-undefined-quot-Error-Workaround
+	*/
 	Ext.override(Ext.ZIndexManager, {
 		tempHidden: [],
 		show: function() {
@@ -65,11 +70,36 @@ Ext.onReady(function () {
 		}
 	});
 	
+	
+	/*
+	Edition en ligne du TreeStore (non utilisé dans ParaCRM
+	http://stackoverflow.com/questions/9076979/extjs4-treepanel-childnode-editing
+			store.indexOf is not a function
+			rowIdx = store.indexOf(record);
+	http://www.sencha.com/forum/showthread.php?130008-Where-did-Ext.tree.TreeEditor-go/page2
+	*/
 	Ext.override(Ext.data.AbstractStore,{
 		indexOf: Ext.emptyFn
 	});
 	
+	
+	
+	/*
+	4.0.2 only??? layout fit grid scrollbar when used does not scroll content -- when filters change
+	http://www.sencha.com/forum/showthread.php?137993-4.0.2-only-layout-fit-grid-scrollbar-when-used-does-not-scroll-content/page4
+	
+	---- en ligne pour chaque gridpanel -------
+	*/
+	
+	
+	
+	
+	/*
+	Désactiver le click droit
+	*/
 	Ext.getBody().on('contextmenu', Ext.emptyFn, null, {preventDefault: true});
+	
+	
 	
 	op5desktop = new Optima5.CoreDesktop.OptimaDesktop ;
 	op5session = new Ext.util.MixedCollection();
