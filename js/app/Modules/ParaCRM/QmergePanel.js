167cdb99c6377e657bed06fafbfe13d691bf056c
Damien Mirand <dm@mirabel-sil.com>
2012-11-12 19:35:02 +0100

    JS - CRMqmerge definition WIP(2)

--- js/app/Modules/ParaCRM/QmergePanel.js
+++ js/app/Modules/ParaCRM/QmergePanel.js
@@ -1,9 +1,39 @@
+Ext.define('QueryModel', {
+	extend: 'Ext.data.Model',
+	idProperty: 'query_id',
+	fields: [
+		{name: 'query_id',  type: 'int'},
+		{name: 'query_name',   type: 'string'},
+		{name: 'target_file_code',   type: 'string'}
+	],
+	hasMany: [{ 
+		model: 'QueryWhereModel',
+		name: 'fields_where',
+		associationKey: 'fields_where'
+	},{
+		model: 'QueryGroupModel',
+		name: 'fields_group',
+		associationKey: 'fields_group'
+	},{
+		model: 'QuerySelectModel',
+		name: 'fields_select',
+		associationKey: 'fields_select'
+	},{
+		model: 'QueryProgressModel',
+		name: 'fields_progress',
+		associationKey: 'fields_progress'
+	}]
+});
+
+
 Ext.define('Optima5.Modules.ParaCRM.QmergePanel' ,{
 	extend: 'Ext.panel.Panel',
 			  
 	alias: 'widget.op5paracrmqmerge',
 			  
 	requires: [] ,
+			  
+	bibleQueriesStore: null,
 			  
 	initComponent: function() {
 		var me = this ;
@@ -46,6 +76,7 @@ Ext.define('Optima5.Modules.ParaCRM.QmergePanel' ,{
 		
 		me.isActive = false ;
 		me.removeAll();
+		me.bibleQueriesStore = null ;
 	},
 
 	qmergeNew: function() {
