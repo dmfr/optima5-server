ad2d895e4524ec89c8b3ccaf45b9a877a2f00977
Damien Mirand <dm@mirabel-sil.com>
2013-01-26 15:47:52 +0100

    Server - Unauthorized media DL (tmp)

---
+++ server/backend_media_DEBUG.php
@@ -0,0 +1,68 @@
+<?php
+$app_root='..' ;
+$server_root='.' ;
+
+include("$server_root/include/config.inc.php");
+
+include("$server_root/modules/media/include/media.inc.php");
+
+
+$domain = 'paramount_prod' ;
+$module_name = $_GET['_moduleName'] ;
+$module_account = $_GET['_moduleAccount'] ;
+
+if( !$domain || !$module_name || !$_GET['media_id'] )
+	die() ;
+elseif( !$module_account )
+	$module_account = 'generic' ;
+
+$media_path = $GLOBALS['media_storage_local_path'].'/'.$domain.'/'.$module_name.'/'.$module_account ;
+if( !is_dir($media_path) )
+	die() ;
+	
+$src_id = $_GET['media_id'] ;
+if( strpos($src_id,'tmp_') === 0 )
+{
+	$ttmp = substr($src_id,4,strlen($src_id)-4) ;
+	$src_path = $media_path.'/tmp/'.$ttmp ;
+}
+else
+{
+	$src_path = $media_path.'/'.$src_id ;
+}
+if( $_GET['thumb'] === true || $_GET['thumb'] == 'true' )
+{
+	$src_path.= '.thumb.jpg' ;
+}
+else
+{
+	$src_path.= '.jpg' ;
+}
+
+if( !is_file($src_path) )
+	die() ;
+	
+if( $_GET['getsize'] === true || $_GET['getsize'] == 'true' )
+{
+	$img_src = imagecreatefromjpeg($src_path);
+	if( $img_src )
+	{
+		$orig_w = imagesx($img_src);
+		$orig_h = imagesy($img_src);
+		die( json_encode( array('success'=>true,'width'=>$orig_w,'height'=>$orig_h) ) );
+	}
+	die( json_encode( array('success'=>false) ) );
+}
+
+if( $_GET['download'] === true || $_GET['download'] == 'true' )
+{
+	$filename = 'OP5.'.rand ( 1000000000 , 9999999999 ).'.jpg' ;
+	header("Content-Type: application/force-download; name=\"$filename\""); 
+	header("Content-Disposition: attachment; filename=\"$filename\""); 
+}
+else
+{
+	header('Content-type: image/jpeg');
+}
+readfile($src_path) ;
+?>
