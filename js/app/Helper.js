ae58a2628fc6974eca507dd7caadf94dd72bf434
Damien Mirand <dm@mirabel-sil.com>
2013-04-24 03:19:33 +0200

    JS - Desktop modules base (dirty)

--- js/app/Helper.js
+++ js/app/Helper.js
@@ -1,5 +1,18 @@
+Ext.define('OptimaModuleModel', {
+	extend: 'Ext.data.Model',
+	idProperty: 'moduleId',
+	fields: [
+		{name: 'moduleId',  type: 'string'},
+		{name: 'moduleName', type:'string'}
+	]
+});
+
+
 Ext.define('Optima5.Helper',{
 	singleton:true,
+	requires:['Ext.data.Store'],
+	
+	modulesStore: null,
 	
 	registerApplication: function( op5CoreApp ) {
 		console.dir( op5CoreApp ) ;
@@ -7,5 +20,36 @@ Ext.define('Optima5.Helper',{
 			  
 	dummyMethod: function(){
 		console.log('Dummy was called !') ;
+	},
+	
+	
+	constructor: function() {
+		var me = this ;
+        console.log('Helper is here!') ;
+		  me.modulesInit() ;
+   },
+	
+	modulesInit: function() {
+		//build store
+		var me = this ;
+		me.modulesStore = Ext.create('Ext.data.Store',{
+			model:'OptimaModuleModel',
+			proxy: {
+				type: 'ajax',
+				url : './js/app/Modules.json',
+				reader: {
+					type: 'json'
+				}
+			},
+			autoLoad: true
+		}) ;
+	},
+	modulesGetById: function( moduleId ) {
+		var me = this ;
+		return me.modulesStore.getById(moduleId) ;
+	},
+	modulesGetAll: function() {
+		var me = this ;
+		return me.modulesStore.getRange() ;
 	}
 });
