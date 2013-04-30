b7950ff880857d54bf1036340be318276565c4ee
Damien Mirand <dm@mirabel-sil.com>
2013-04-30 18:54:17 +0200

    Wallpaper service

---
+++ wallpapers/wallpaper.php
@@ -0,0 +1,38 @@
+<?php
+$app_root='..' ;
+$server_root=$app_root.'/server' ;
+
+include("$server_root/backend_checksession.inc.php") ;
+
+include("$server_root/include/config.inc.php");
+
+include("$server_root/modules/media/include/media.inc.php");
+
+
+$domain = $_SESSION['login_data']['login_domain'] ;
+
+if( !$domain )
+	die() ;
+
+$media_path = $GLOBALS['media_storage_local_path'].'/'.$domain.'/'.'__wallpapers' ;
+if( !is_dir($media_path) )
+	die() ;
+	
+$src_id = $_GET['media_id'] ;
+if( $src_id = $_GET['wallpaper_id'] )
+{
+	$src_path = $media_path.'/'.$src_id ;
+	$src_path.= '.jpg' ;
+}
+else
+{
+	$src_path = $app_root.'/wallpapers/default.jpg' ;
+}
+
+if( !is_file($src_path) )
+	die() ;
+
+header('Content-type: image/jpeg');
+readfile($src_path) ;
+die();
+?>
