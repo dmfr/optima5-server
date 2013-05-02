c3bac4261461b4c349139fb011c7f8506a337588
Damien Mirand <dm@mirabel-sil.com>
2013-05-02 20:24:13 +0200

    Server - Desktop module (WIP 130502)

--- server/include/config.DB.inc.php
+++ server/include/config.DB.inc.php
@@ -1,4 +1,9 @@
-<?
+<?php
+	// ** SHA1 salt : Constant, changing it voids user database !
+	define('AUTH_SHA1_SALT','zo5propvkhj6cz4a8tvbudsoeuio') ;
+	// ** Root password / Recovery backdoor (should be empty for secure production!) **
+	define('AUTH_ROOT_PASSWORD_PLAIN','') ;
+	
 	$mysql_host = 'localhost' ;
 	$mysql_user = 'root' ;
 	$mysql_pass = '' ;
--- server/modules/desktop/include/desktop_config.inc.php
+++ server/modules/desktop/include/desktop_config.inc.php
@@ -8,13 +8,23 @@ function desktop_config_getRecord($post_data) {
 	}
 	
 	$session_id = $_SESSION['login_data']['session_id'] ;
-	$user_code = $_SESSION['login_data']['user_code'] ;
-
+	$user_code = $_SESSION['login_data']['login_user'] ;
 	
+	$login_userName = $_opDB->query_uniqueValue("SELECT user_fullname FROM auth_user WHERE user_code='$user_code'") ;
+	$login_domainName = $_opDB->query_uniqueValue("SELECT domain_name FROM domain WHERE zero_id='0'") ;
 	
 	return array(
+		'desktop_config'=>array(
+			'session_id' => $_SESSION['login_data']['session_id'],
+			'dev_mode' => $GLOBALS['__OPTIMA_TEST'],
+			'auth_is_admin' => ($_SESSION['login_data']['auth_class']=='A'),
+			'auth_is_root' => ($user_code=='root'),
+			'login_str' => $_SESSION['login_data']['userstr'],
+			'login_userName' => $login_userName,
+			'login_domainName' => $login_domainName
+		),
 		'success'=>true
-		);
+	);
 }
 
 ?>
