ff83985c5cdbdc560b90931edafab5626c3fed1a
Damien Mirand <dm@mirabel-sil.com>
2013-07-05 14:39:06 +0200

    ExtJS 4.1 - remove app.js overrides

--- app.js
+++ app.js
@@ -53,24 +53,6 @@ Ext.onReady(function () {
 		}
 	});
 
-	/*
-	Problème : erreur au destroy + recréation du FilePanel / BiblePanel
-	"this.tempHidden is undefined" Error Workaround 
-	http://www.sencha.com/forum/showthread.php?160222-quot-this.tempHidden-is-undefined-quot-Error-Workaround
-	*/
-	Ext.override(Ext.ZIndexManager, {
-		tempHidden: [],
-		show: function() {
-			var comp, x, y;
-			while (comp = this.tempHidden.shift()) {
-				x = comp.x;
-				y = comp.y;
-				comp.show();
-				comp.setPosition(x, y);
-			}
-		}
-	});
-	
 	
 	/*
 	Edition en ligne du TreeStore (non utilisé dans ParaCRM
@@ -83,14 +65,6 @@ Ext.onReady(function () {
 		indexOf: Ext.emptyFn
 	});
 	
-	
-	
-	/*
-	4.0.2 only??? layout fit grid scrollbar when used does not scroll content -- when filters change
-	http://www.sencha.com/forum/showthread.php?137993-4.0.2-only-layout-fit-grid-scrollbar-when-used-does-not-scroll-content/page4
-	
-	---- en ligne pour chaque gridpanel -------
-	*/
 	
 	
 	
@@ -118,49 +92,6 @@ Ext.onReady(function () {
 	Ext.form.FieldSet.override( {
 		beforeDestroy: function() {
 			this.callParent() ;
-		}
-	});
-	
-	Ext.view.DropZone.override( {
-		onNodeDrop: function(node, dragZone, e, data) {
-			var me = this,
-					dropHandled = false,
-
-					// Create a closure to perform the operation which the event handler may use.
-					// Users may now set the wait parameter in the beforedrop handler, and perform any kind
-					// of asynchronous processing such as an Ext.Msg.confirm, or an Ajax request,
-					// and complete the drop gesture at some point in the future by calling either the
-					// processDrop or cancelDrop methods.
-					dropHandlers = {
-						wait: false,
-						processDrop: function () {
-							me.invalidateDrop();
-							me.handleNodeDrop(data, me.overRecord, me.currentPosition);
-							dropHandled = true;
-							me.fireViewEvent('drop', node, data, me.overRecord, me.currentPosition);
-						},
-
-						cancelDrop: function() {
-							me.invalidateDrop();
-							dropHandled = true;
-						}
-					},
-					performOperation = false;
-
-			if (me.valid) {
-					performOperation = me.fireViewEvent('beforedrop', node, data, me.overRecord, me.currentPosition, dropHandlers);
-					if (dropHandlers.wait) {
-						return;
-					}
-
-					if (performOperation !== false) {
-						// If either of the drop handlers were called in the event handler, do not do it again.
-						if (!dropHandled) {
-							dropHandlers.processDrop();
-						}
-					}
-			}
-			return performOperation;
 		}
 	});
 	
@@ -213,27 +144,8 @@ Ext.onReady(function () {
 			this.updateOperation.apply(this, arguments);
 		}
 	});
-	// Missing private "onResize" call in Ext 4.0.7
-	Ext.AbstractComponent.override({
-		afterComponentLayout: function(width, height, isSetSize, callingContainer) {
-			this.callOverridden(arguments) ;
-			
-			var oldWidth = 0,
-				oldHeight = 0 ;
-			if( this.preLayoutSize ) {
-				oldWidth = this.preLayoutSize.width ;
-				oldHeight = this.preLayoutSize.height ;
-			}
-			
-			this.onResize(width, height, oldWidth, oldHeight);
-		}
-	});
 	
 	
-	
-	/*
-	 * From ExtJS 4.1 : fix EXTJSIV-5383 bug in Ext.dd.DragDropManager
-	 */
 	
 	
 	
