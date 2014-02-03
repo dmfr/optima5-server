ace304e91f418e528cb79cd623e32f25bdd0e94a
Damien Mirand <dm@mirabel-sil.com>
2014-02-03 23:12:47 +0100

    JS - ux.dams.FieldSet (FieldSet w/ icon)

---
+++ js/ux/dams/FieldSet.js
@@ -0,0 +1,63 @@
+Ext.define('Ext.ux.dams.FieldSet',{
+	extend:'Ext.form.FieldSet',
+	alias: 'widget.damsfieldset',
+	
+	initComponent: function() {
+		this.callParent() ;
+		this.addCls(this.baseCls + '-withicon') ;
+		this.addEvents('iconclick') ;
+	},
+    createLegendCt: function () {
+        var me = this,
+            items = [],
+            legend = {
+                xtype: 'container',
+                baseCls: me.baseCls + '-header',
+                id: me.id + '-legend',
+                autoEl: 'legend',
+                items: items,
+                ownerCt: me,
+                ownerLayout: me.componentLayout
+            };
+
+        // Checkbox
+        if (me.checkboxToggle) {
+            items.push(me.createCheckboxCmp());
+        } else if (me.collapsible) {
+            // Toggle button
+            items.push(me.createToggleCmp());
+        }
+
+        // Icon
+        items.push(me.createIconCmp());
+        // Title
+        items.push(me.createTitleCmp());
+
+        return legend;
+    },
+    createIconCmp: function() {
+        var me  = this,
+            cfg = {
+                xtype : 'component',
+                html  : '&#160;',
+                cls   : me.baseCls + '-header-icon',
+                id    : me.id + '-legendIcon'
+            };
+
+        if (true) {
+            cfg.listeners = {
+                el : {
+                    scope : me,
+                    click : me.iconClick
+                }
+            };
+            cfg.cls += ' ' + me.iconCls;
+        }
+
+        return (me.iconCmp = Ext.widget(cfg));
+    },
+	 iconClick: function(e) {
+		 this.fireEvent('iconclick',this,e) ;
+	 }
+	 
+});
