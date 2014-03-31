7ff186f6cd18605561e54d4376df92cfbbb4ff91
Damien Mirand <dm@mirabel-sil.com>
2014-03-31 14:28:22 +0200

    ExtJS 4.2 - CRM MainWindowButton

--- js/app/Modules/CrmBase/MainWindowButton.js
+++ js/app/Modules/CrmBase/MainWindowButton.js
@@ -2,19 +2,38 @@ Ext.define('Optima5.Modules.CrmBase.MainWindowButton',{
 	extend:'Ext.button.Button',
 	alias: 'widget.op5crmbasemwbutton',
 	
-	renderTpl:
-		'<em id="{id}-btnWrap" class="{splitCls}">' +
-			'<button id="{id}-btnEl" type="{type}" class="{btnCls}" hidefocus="true" role="button" autocomplete="off">' +
-				'<span id="{id}-btnInnerEl" class="{baseCls}-inner" style="{innerSpanStyle}">' +
-					'<span id="{id}-btnInnerTextTitle" class="op5-crmmbase-mainwindowbtn-title">{textTitle}</span>' +
-					'&nbsp;&nbsp;' +
-					'<span id="{id}-btnInnerTextRedcount" class="op5-crmmbase-mainwindowbtn-redcount">({textRedcount})</span>' +
-					'<br/>' +
-					'<span id="{id}-btnInnerTextCaption" class="op5-crmmbase-mainwindowbtn-caption">{textCaption}</span>' +
-				'</span>' +
-				'<span id="{id}-btnIconEl" class="{baseCls}-icon {iconCls}">&#160;</span>' +
-			'</button>' +
-		'</em>' ,
+	renderTpl: [
+		'<span id="{id}-btnWrap" role="presentation" class="{baseCls}-wrap',
+			'<tpl if="splitCls"> {splitCls}</tpl>',
+			'{childElCls}" unselectable="on">',
+			'<span id="{id}-btnEl" class="{baseCls}-button" role="presentation">',
+				'<span id="{id}-btnInnerEl" class="{baseCls}-inner {innerCls}',
+					'{childElCls}" unselectable="on">',
+						'<span id="{id}-btnInnerTextTitle" class="op5-crmmbase-mainwindowbtn-title">{textTitle}</span>',
+						'&nbsp;&nbsp;',
+						'<span id="{id}-btnInnerTextRedcount" class="op5-crmmbase-mainwindowbtn-redcount">({textRedcount})</span>',
+						'<br/>',
+						'<span id="{id}-btnInnerTextCaption" class="op5-crmmbase-mainwindowbtn-caption">{textCaption}</span>',
+				'</span>',
+				'<span role="presentation" id="{id}-btnIconEl" class="{baseCls}-icon-el {iconCls}',
+					'{childElCls} {glyphCls}" unselectable="on" style="',
+					'<tpl if="iconUrl">background-image:url({iconUrl});</tpl>',
+					'<tpl if="glyph && glyphFontFamily">font-family:{glyphFontFamily};</tpl>">',
+					'<tpl if="glyph">&#{glyph};</tpl><tpl if="iconCls || iconUrl">&#160;</tpl>',
+				'</span>',
+			'</span>',
+		'</span>',
+		// if "closable" (tab) add a close element icon
+		'<tpl if="closable">',
+			'<span id="{id}-closeEl" role="presentation"',
+					' class="{baseCls}-close-btn"',
+					'<tpl if="closeText">',
+						' title="{closeText}" aria-label="{closeText}"',
+					'</tpl>',
+					'>',
+			'</span>',
+		'</tpl>'
+	],
 	
 	initComponent: function() {
 		var me = this ;
