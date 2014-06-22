12ea060686ac5ddfc6090396a16b1897483b4c2f
Damien Mirand <dm@mirabel-sil.com>
2014-06-23 00:36:17 +0200

    Spec DBS_PEOPLE - ToDo 140621, RealPanel print

--- js/app/Modules/Spec/DbsPeople/RealPanel.js
+++ js/app/Modules/Spec/DbsPeople/RealPanel.js
@@ -276,6 +276,13 @@ Ext.define('Optima5.Modules.Spec.DbsPeople.RealPanel',{
 				},
 				scope: this
 			},{
+				icon: 'images/op5img/ico_print_16.png',
+				text: 'Print',
+				handler: function(btn) {
+					this.handlePrintPanel() ;
+				},
+				scope: this
+			},{
 				icon: 'images/op5img/ico_calendar_16.png',
 				text: 'Choix Semaine',
 				menu: Ext.create('Ext.menu.DatePicker',{
@@ -1771,6 +1778,66 @@ Ext.define('Optima5.Modules.Spec.DbsPeople.RealPanel',{
 		}
 	},
 	
+	/*
+	 * Borrowed from http://druckit.wordpress.com/2014/02/15/ext-js-4-printing-the-contents-of-an-ext-panel/
+	 */
+	handlePrintPanel: function() {
+		var pnl = this.down('grid') ;
+		
+		// instantiate hidden iframe
+		var iFrameId = "printerFrame";
+		var printFrame = Ext.get(iFrameId);
+
+		if (printFrame == null) {
+				printFrame = Ext.getBody().appendChild({
+					id: iFrameId,
+					tag: 'iframe',
+					cls: 'x-hidden',
+					style: {
+						display: "none"
+					}
+				});
+		}
+
+		var cw = printFrame.dom.contentWindow;
+
+		// instantiate application stylesheets in the hidden iframe
+		var stylesheets = "";
+		for (var i = 0; i < document.styleSheets.length; i++) {
+				stylesheets += Ext.String.format('<link rel="stylesheet" href="{0}" />', document.styleSheets[i].href);
+		}
+
+		// various style overrides
+		stylesheets += ''.concat(
+			"<style>",
+				".x-panel-body {overflow: visible !important;}",
+				// experimental - page break after embedded panels
+				// .x-panel {page-break-after: always; margin-top: 10px}",
+			"</style>"
+			);
+
+		// get the contents of the panel and remove hardcoded overflow properties
+		var markup = pnl.getEl().dom.innerHTML;
+		while (markup.indexOf('overflow: auto;') >= 0) {
+				markup = markup.replace('overflow: auto;', '');
+		}
+
+		var str = Ext.String.format('<html><head>{0}</head><body>{1}</body></html>',stylesheets,markup);
+		
+		// output to the iframe
+		cw.document.open();
+		cw.document.write(str);
+		cw.document.close();
+
+		// remove style attrib that has hardcoded height property
+		cw.document.getElementsByTagName('DIV')[0].removeAttribute('style');
+
+		// print the iframe
+		cw.print();
+
+		// destroy the iframe
+		Ext.fly(iFrameId).destroy();
+	},
 	
 	handleQuit: function() {
 		this.destroy() ;
