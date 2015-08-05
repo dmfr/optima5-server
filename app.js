3e96ac529ed4833f5ed004b64fc0a7f91ae37f2f
DM Laptop <dm@mirabel-sil.com>
2015-08-05 17:01:37 +0200

    JS - Prevent BACKSPACE default

--- app.js
+++ app.js
@@ -154,8 +154,13 @@ Ext.onReady(function () {
 	/*
 	Désactiver le click droit
 	*/
-	Ext.getDoc().on('contextmenu', function(ev){
-		ev.preventDefault() ;
+	Ext.getDoc().on('contextmenu', function(e){
+		e.preventDefault() ;
+	}) ;
+	Ext.getDoc().on('keydown', function(e){
+		if( e.getKey() == e.BACKSPACE && !Ext.Array.contains(['text','password','textarea'], e.getTarget().type) ) {
+			e.preventDefault();
+		}
 	}) ;
 	
 	// onReady : bootstrap Optima app.
