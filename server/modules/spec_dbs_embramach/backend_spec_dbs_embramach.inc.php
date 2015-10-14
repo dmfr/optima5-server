9707700cb0375429acd5660c2078ef23b639d599
DM Laptop <dm@mirabel-sil.com>
2015-10-14 14:35:57 +0200

    Spec DBS_EMBRAMACH - Refactor upload

--- server/modules/spec_dbs_embramach/backend_spec_dbs_embramach.inc.php
+++ server/modules/spec_dbs_embramach/backend_spec_dbs_embramach.inc.php
@@ -1,7 +1,6 @@
 <?php
 include("$server_root/modules/paracrm/include/paracrm.inc.php") ;
 include("$server_root/modules/spec_dbs_embramach/include/specDbsEmbramach.inc.php") ;
-include("$server_root/modules/spec_dbs_embramach/include/specDbsEmbramach_stats.inc.php") ;
 
 
 function backend_specific( $post_data )
@@ -18,8 +17,9 @@ switch( $post_data['_action'] )
 	case 'mach_saveGridRow' :
 	return specDbsEmbramach_mach_saveGridRow( $post_data ) ;
 	
+	case 'upload' :
 	case 'mach_upload' :
-	return specDbsEmbramach_mach_upload( $post_data ) ;
+	return specDbsEmbramach_upload( $post_data ) ;
 	
 	case 'stats_getPicking' :
 	return specDbsEmbramach_stats_getPicking( $post_data ) ;
