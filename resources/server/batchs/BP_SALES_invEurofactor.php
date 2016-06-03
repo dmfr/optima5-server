bf3c49316959995ef33d550ae304a16a828b1cc6
Damien Mirand <dm@mirabel-sil.com>
2016-06-03 16:47:03 +0200

    Resources - BP EuroFactor (v.0.01)

---
+++ resources/server/batchs/BP_SALES_invEurofactor.php
@@ -0,0 +1,125 @@
+<?php
+session_start() ;
+
+$app_root = dirname($_SERVER['SCRIPT_NAME']).'/../../..' ;
+$server_root=$app_root.'/server' ;
+$resources_root=$app_root.'/resources' ;
+$templates_dir=$resources_root.'/server/templates' ;
+
+@include_once 'PHPExcel/PHPExcel.php' ;
+
+include("$server_root/include/config.inc.php");
+include("$server_root/include/toolfunctions.inc.php");
+include("$server_root/modules/media/include/media.inc.php");
+
+include( "$server_root/include/database/mysql_DB.inc.php" ) ;
+$_opDB = new mysql_DB( );
+$_opDB->connect_mysql( $mysql_host, $mysql_db, $mysql_user, $mysql_pass );
+$_opDB->query("SET NAMES UTF8") ;
+
+include("$server_root/modules/paracrm/backend_paracrm.inc.php");
+
+
+include('BP_SALES_invEurofactor_mailFactory.inc.php') ;
+
+
+
+
+// *********** Get CONSTANTES *******************
+$GLOBALS['factor_ref'] = date('ymdHis') ;
+
+$GLOBALS['_cfg_peer_code'] = 'INV_EUROFACTOR' ;
+
+$query = "SELECT field_MAIL_SENDTO, field_PARAMS FROM view_bible_CFG_PEER_entry
+	WHERE entry_key='{$GLOBALS['_cfg_peer_code']}'" ;
+$result = $_opDB->query($query) ;
+if( $_opDB->num_rows($result) != 1 ) {
+	die() ;
+}
+$arr = $_opDB->fetch_row($result) ;
+
+$field_params = $arr[1] ;
+$field_sendto = $arr[0] ;
+
+$GLOBALS['arr_mailinglist'] = array() ;
+foreach( explode(',',$field_sendto) as $email ) {
+	$GLOBALS['arr_mailinglist'][] = trim($email) ;
+}
+
+foreach( explode(',',$field_params) as $param ) {
+	$ttmp = explode('=',$param) ;
+	if( count($ttmp) != 2 ) {
+		continue ;
+	}
+	$mkey = trim($ttmp[0]) ;
+	$mval = trim($ttmp[1]) ;
+	switch( $mkey ) {
+		case 'E' :
+			$GLOBALS['factor_emetteur'] = $mval ;
+			break ;
+		case 'C' :
+			$GLOBALS['factor_client'] = $mval ;
+			break ;
+		case 'AFC' :
+			$GLOBALS['factor_AFC'] = $mval ;
+			break ;
+		default :
+			break ;
+	}
+}
+// *******************************************
+
+
+$arr_invFilerecordIds = array() ;
+
+// ************ Chargement INV **************
+$query = "SELECT i.filerecord_id FROM view_file_INV i
+	LEFT OUTER JOIN view_file_INV_PEER ip ON ip.filerecord_parent_id=i.filerecord_id
+		AND ip.field_PEER_CODE='{$GLOBALS['_cfg_peer_code']}'
+	WHERE i.field_STATUS_IS_FINAL='1' and (ip.field_SEND_IS_OK IS NULL OR ip.field_SEND_IS_OK<>'1')
+	AND i.field_CALC_AMOUNT_FINAL > '0'" ;
+$result = $_opDB->query($query) ;
+while( ($arr = $_opDB->fetch_row($result)) != FALSE ) {
+	$filerecord_id = $arr[0] ;
+	$arr_invFilerecordIds[] = $filerecord_id ;
+}
+if( !$arr_invFilerecordIds ) {
+	exit ;
+}
+
+// ************* Constitution de l'envoi ************
+$email_text = mail_getBody( $arr_invFilerecordIds ) ;
+$binarybuffer_xlsx = mail_getBinary_remiseTxt( $arr_invFilerecordIds ) ;
+$to = $arr_mailinglist ;
+if( $GLOBALS['__OPTIMA_TEST'] ) {
+	$to = array() ;
+	$to[] = 'dm@mirabel-sil.com' ;
+}
+
+$email = new Email() ;
+$email->set_From( 'finance@bluephoenix.fr', 'BluePhoenix Finance' ) ;
+foreach( $to as $to_email ) {
+	$email->add_Recipient( $to_email ) ;
+}
+$email->set_Subject( '[BluePhoenix] '.'Remise factures du'.' '.date('d/m/Y').' '.'à'.' '.date('H:i') ) ;
+$email->set_text_body( $email_text ) ;
+$email->attach_file( "FAA{$GLOBALS['factor_emetteur']}".'_'.$GLOBALS['factor_ref'].'.txt', $binarybuffer_xlsx, 'plain/text' ) ;
+$email->send() ;
+// ***************************************************
+
+
+
+// ****** Update CRM database ********
+foreach( $arr_invFilerecordIds as $inv_filerecord_id ) {
+	$arr_ins = array() ;
+	$arr_ins['field_PEER_CODE'] = $GLOBALS['_cfg_peer_code'] ;
+	$arr_ins['field_SEND_IS_OK'] = 1 ;
+	$arr_ins['field_SEND_REF'] = $GLOBALS['factor_ref'] ;
+	$arr_ins['field_SEND_DATE'] = date('Y-m-d H:i:s') ;
+	paracrm_lib_data_insertRecord_file( 'INV_PEER' , $inv_filerecord_id , $arr_ins ) ;
+}
+
+
+
+
+?>
---
+++ resources/server/batchs/BP_SALES_invEurofactor_mailFactory.inc.php
@@ -0,0 +1,106 @@
+<?php
+function mail_getBinary_remiseTxt( $arr_invFilerecordIds ) { // return String (binarybuffer)
+	global $_opDB ;
+	
+	$buffer = '' ;
+	foreach( $arr_invFilerecordIds as $inv_filerecord_id ) {
+		$query = "SELECT * FROM view_file_INV WHERE filerecord_id='{$inv_filerecord_id}'" ;
+		$result = $_opDB->query($query) ;
+		$arr = $_opDB->fetch_assoc($result) ;
+		
+		$lig = '' ;
+		$lig = substr_mklig($lig,$GLOBALS['factor_emetteur'],0,5) ;
+		$lig = substr_mklig($lig,';',5,1) ;
+		$lig = substr_mklig($lig,$GLOBALS['factor_client'],6,5) ;
+		$lig = substr_mklig($lig,';',11,1) ;
+		$lig = substr_mklig($lig,date('Ymd'),12,8) ;
+		$lig = substr_mklig($lig,';',20,1) ;
+		$lig = substr_mklig($lig,'D',21,1) ;
+		$lig = substr_mklig($lig,';',22,1) ;
+		$lig = substr_mklig($lig,$GLOBALS['factor_AFC'],23,3) ;
+		$lig = substr_mklig($lig,';',26,1) ;
+		$lig = substr_mklig($lig,'F',27,1) ;
+		$lig = substr_mklig($lig,';',28,1) ;
+		$lig = substr_mklig($lig,'EUR',29,3) ;
+		$lig = substr_mklig($lig,';',32,1) ;
+		$lig = substr_mklig($lig,'0000000',33,7) ;
+		$lig = substr_mklig($lig,';',40,1) ;
+		$lig = substr_mklig($lig,$arr['field_CLI_LINK'],41,15) ;
+		$lig = substr_mklig($lig,';',56,1) ;
+		$lig = substr_mklig($lig,'',57,23) ;
+		$lig = substr_mklig($lig,';',80,1) ;
+		$lig = substr_mklig($lig,preg_replace("/[^a-zA-Z0-9]/", "",$arr['field_ID_INV']),81,14) ;
+		$lig = substr_mklig($lig,';',95,1) ;
+		$lig = substr_mklig($lig,str_pad(round(((float)$arr['field_CALC_AMOUNT_FINAL'])*100), 15, "0", STR_PAD_LEFT),96,15) ;
+		$lig = substr_mklig($lig,';',111,1) ;
+		$lig = substr_mklig($lig,date('Ymd',strtotime($arr['field_DATE_INVOICE'])),112,8) ;
+		$lig = substr_mklig($lig,';',120,1) ;
+		$lig = substr_mklig($lig,date('Ymd',strtotime('+30 days',strtotime($arr['field_DATE_INVOICE']))),121,8) ;
+		$lig = substr_mklig($lig,';',129,1) ;
+		$lig = substr_mklig($lig,'A',130,1) ;
+		$lig = substr_mklig($lig,';',131,1) ;
+		$lig = substr_mklig($lig,$arr['field_ID_CDE_REF'],132,10) ;
+		$lig = substr_mklig($lig,';',142,1) ;
+		$lig = substr_mklig($lig,'',143,25) ;
+		$lig = substr_mklig($lig,';',168,1) ;
+		$lig = substr_mklig($lig,'',169,14) ;
+		$lig = substr_mklig($lig,';',183,1) ;
+		$lig = substr_mklig($lig,'',184,51) ;
+		$lig = substr_mklig($lig,';',235,1) ;
+		$lig = substr_mklig($lig,'',236,3) ;
+		$lig = substr_mklig($lig,';',239,1) ;
+		$buffer.= $lig."\r\n" ;
+	}
+	return $buffer ;
+}
+
+function mail_getBody( $arr_invFilerecordIds ) {
+	global $_opDB ;
+	
+	
+	
+	//print_r($file_CDE_SAISIE) ;
+	//print_r($bible_STORE_entry) ;
+	//print_r($TABfile_CDE_SAISIE_LIG) ;
+	$email_text = "" ;
+	$email_text.= "Société        : ".'Blue Phoenix, 1 cité Paradis, 75010 PARIS'."\r\n" ;
+	$email_text.= "Factor         : ".'CA Eurofactor'."\r\n" ;
+	$email_text.= "\r\n" ;
+	$email_text.= "Date remise    : ".date('Y-m-d')."\r\n" ;
+	$email_text.= "Ref  remise    : ".$GLOBALS['factor_ref']."\r\n" ;
+	$email_text.= "Devise         : ".'EUR'."\r\n" ;
+	$email_text.= "\r\n" ;
+	$email_text.= "  Client       |  Date  |  Facture  |  Montant  | Echeance | Ref.cde  "."\r\n" ;
+	$email_text.= "---------------|--------|-----------|-----------|----------|----------"."\r\n" ;
+	
+	$email_base = "               |        |           |           |          |          " ;
+	
+	foreach( $arr_invFilerecordIds as $inv_filerecord_id ) {
+		$query = "SELECT * FROM view_file_INV WHERE filerecord_id='{$inv_filerecord_id}'" ;
+		$result = $_opDB->query($query) ;
+		$arr = $_opDB->fetch_assoc($result) ;
+		
+		$lig = $email_base ;
+		$lig = substr_mklig($lig,$arr['field_CLI_LINK'],0,15) ;
+		$lig = substr_mklig($lig,date('d/m/y',strtotime($arr['field_DATE_INVOICE'])),16,8) ;
+		$lig = substr_mklig($lig,preg_replace("/[^a-zA-Z0-9]/", "",$arr['field_ID_INV']),25,11) ;
+		$lig = substr_mklig($lig,number_format(round($arr['field_CALC_AMOUNT_FINAL'],2),2),37,10,true) ;
+		$lig = substr_mklig($lig,date('d/m/y',strtotime('+30 days',strtotime($arr['field_DATE_INVOICE']))),50,8) ;
+		$lig = substr_mklig($lig,$arr['field_ID_CDE_REF'],60,10) ;
+		$email_text.= $lig."\r\n" ;
+		
+		$total += round($arr['field_CALC_AMOUNT_FINAL'],2) ;
+	}
+	$email_text.= $email_base."\r\n" ;
+	
+	$email_text.= "\r\n" ;
+	
+	$lig = "Total remise TTC :" ;
+	$lig = substr_mklig($lig,number_format($total,2),37,10,true) ;
+	$email_text.= $lig."\r\n" ;
+	
+	$email_text.= "\r\n" ;
+	
+	return $email_text ;
+}
+?>
