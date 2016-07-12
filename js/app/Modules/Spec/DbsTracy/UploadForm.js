86a5e82878d57cb300cc4ddeb74335f840508223
Damien Mirand <dm@mirabel-sil.com>
2016-07-12 04:17:05 +0200

    Spec DBS_TRACY - Upload RCL_SPEC

--- js/app/Modules/Spec/DbsTracy/UploadForm.js
+++ js/app/Modules/Spec/DbsTracy/UploadForm.js
@@ -44,7 +44,7 @@ Ext.define('Optima5.Modules.Spec.DbsTracy.UploadForm',{
 					editable: false,
 					store: {
 						fields: ['id'],
-						data: [{id:'RCL_VL06F'},{id:'MBD_VL06F'},{id:'MBD_LIKP'}]
+						data: [{id:'RCL_VL06F'},{id:'MBD_VL06F'},{id:'MBD_LIKP'},{id:'RCL_SPEC'}]
 					},
 					valueField: 'id',
 					displayField: 'id'
--- server/modules/spec_dbs_tracy/include/specDbsTracy_upload.inc.php
+++ server/modules/spec_dbs_tracy/include/specDbsTracy_upload.inc.php
@@ -7,6 +7,9 @@ function specDbsTracy_upload( $post_data ) {
 	
 	// Specs
 	switch( $file_model ) {
+		case 'RCL_SPEC' :
+			$ret = specDbsTracy_upload_RCLSPEC_tmp($handle,'ACL') ;
+			break ;
 		case 'RCL_VL06F' :
 			$ret = specDbsTracy_upload_VL06F_tmp($handle,'ACL') ;
 			break ;
@@ -21,6 +24,194 @@ function specDbsTracy_upload( $post_data ) {
 	}
 	
 	return array('success'=>$ret) ;
+}
+
+function specDbsTracy_upload_RCLSPEC_tmp( $handle, $id_soc ) {
+	global $_opDB ;
+	
+	$handle_priv = tmpfile();
+	specDbsTracy_upload_lib_separator($handle,$handle_priv) ;
+	fseek($handle_priv,0) ;
+	
+	$map_idSoc_idDn_torf = array() ;
+	$query = "SELECT field_ID_SOC, field_ID_DN FROM view_file_CDE" ;
+	$result = $_opDB->query($query) ;
+	while( ($arr = $_opDB->fetch_row($result)) != FALSE ) {
+		$map_idSoc_idDn_torf[$arr[0]][$arr[1]] = true ;
+	}
+	
+	fgets($handle_priv) ;
+	while( !feof($handle_priv) ) {
+		$arr_csv = fgetcsv($handle_priv) ;
+		if( !$arr_csv ) {
+			continue ;
+		}
+		foreach( $arr_csv as &$tvalue ) {
+			$tvalue = trim($tvalue) ;
+		}
+		unset($tvalue) ;
+		
+		
+		$form_data = array() ;
+		$form_data['id_soc'] = 'ACL' ;
+		
+		if( $form_data['id_soc'] != $id_soc ) {
+			continue ;
+		}
+		$passed = true ;
+		$form_data['id_dn'] = $arr_csv[0] ;
+		$form_data['flow_code'] = 'AIR' ; //TODO: dynamic ??
+		$form_data['atr_type'] = 'STD' ;
+		
+		// Map des statuts
+		$mkey = $form_data['id_soc'].'%%%'.$form_data['id_dn'] ;
+		$map_id_statuses[$mkey] = array(
+			'SGP' => $arr_csv[14],
+			'SM' => $arr_csv[15]
+		) ;
+		
+		if( $map_idSoc_idDn_torf[$form_data['id_soc']][$form_data['id_dn']] ) {
+			continue ;
+		}
+		
+		// Create CONSIGNEE ?
+		$query = "SELECT entry_key FROM view_bible_LIST_CONSIGNEE_entry WHERE field_NAME='{$arr_csv[12]}'" ;
+		$atr_consignee = $_opDB->query_uniqueValue($query) ;
+		if( !$atr_consignee ) {
+			$entry_key = preg_replace("/[^a-zA-Z0-9]/", "", strtoupper($arr_csv[12])) ;
+		
+			$arr_ins = array() ;
+			$arr_ins['field_CODE'] = $entry_key ;
+			$arr_ins['field_NAME'] = $arr_csv[12] ;
+			paracrm_lib_data_insertRecord_bibleEntry( 'LIST_CONSIGNEE', $entry_key, 'UPLOAD', $arr_ins ) ;
+			
+			$atr_consignee = $entry_key ;
+		}
+		$form_data['atr_consignee'] = $atr_consignee ;
+		
+		// Fetch UPLOAD_RECEP ?
+		$query = "SELECT * FROM view_bible_UPLOAD_RECEP_entry WHERE treenode_key='{$form_data['id_soc']}' AND entry_key='{$arr_csv[11]}'" ;
+		$result = $_opDB->query($query) ;
+		if( $_opDB->num_rows($result) == 1 ) {
+			$arr_recep = $_opDB->fetch_assoc($result) ;
+			
+			$txt_location_full=array() ;
+			foreach( array('field_ADR_NAME1','field_ADR_NAME2','field_ADR_STREET','field_ADR_CITY','field_ADR_COUNTRY') as $mkey ) {
+				if( $arr_recep[$mkey] ) {
+					$txt_location_full[] = $arr_recep[$mkey] ;
+				}
+			}
+			$form_data['txt_location_full'] = implode("\n",$txt_location_full) ;
+		} else {
+			$form_data['txt_location_full'] = $arr_csv[12]."\n".$arr_csv[13] ;
+		}
+		$form_data['txt_location_city'] = $arr_csv[13] ;
+		
+		$form_data['atr_priority'] = 3 ;
+		$form_data['atr_incoterm'] = $arr_csv[10] ;
+		
+		$json_return = specDbsTracy_order_setHeader(array(
+			'_is_new' => true,
+			'data' => json_encode($form_data)
+		));
+		$filerecord_id = $json_return['id'] ;
+		
+		$p_dateRaw = $arr_csv[5] ;
+		$p_dateSM = substr($p_dateRaw,6,4).'-'.substr($p_dateRaw,3,2).'-'.substr($p_dateRaw,0,2) ;
+		
+		$arr_cond = array() ;
+		$arr_cond['filerecord_id'] = $filerecord_id ;
+		$arr_update = array() ;
+		$arr_update['field_DATE_CREATE'] = date('Y-m-d H:i:s') ;
+		$arr_update['field_DATE_INIT'] = $p_dateSM ;
+		$_opDB->update('view_file_CDE',$arr_update,$arr_cond) ;
+		
+		
+		
+		// Date
+		$str_date = $arr_csv[3] ;
+		$str_time = $arr_csv[4] ;
+		$p_dateRelease = substr($str_date,6,4).'-'.substr($str_date,3,2).'-'.substr($str_date,0,2).' '.$str_time ;
+		
+		
+		$arr_cond = array() ;
+		$arr_cond['filerecord_parent_id'] = $filerecord_id ;
+		$arr_cond['field_STEP_CODE'] = '10_RLS' ;
+		$arr_update = array() ;
+		$arr_update['field_DATE_ACTUAL'] = $p_dateRelease ;
+		$arr_update['field_STATUS_IS_OK'] = 1 ;
+		$_opDB->update('view_file_CDE_STEP',$arr_update,$arr_cond) ;
+	}
+	
+	fclose($handle_priv) ;
+	
+	
+	if( !$passed ) {
+		return $passed ;
+	}
+	
+	
+	$ttmp_json = specDbsTracy_order_getRecords( array(
+		'filter_socCode' => $form_data['id_soc']
+	));
+	$rows_order = $ttmp_json['data'] ;
+	foreach( $rows_order as $row_order ) {
+		$statuses = $map_id_statuses[$row_order['id_soc'].'%%%'.$row_order['id_dn']] ;
+		if( !$statuses ) {
+			continue ;
+		}
+		
+		// Sans OT ?
+		$no_OT = ( $statuses['SGP']=='A' && $statuses['SM']=='A' );
+		if( $no_OT && !$row_order['warning_is_on'] ) {
+			// MeP warning
+			specDbsTracy_order_setWarning( array(
+				'order_filerecord_id' => $row_order['order_filerecord_id'],
+				'data' => json_encode(array(
+					'warning_is_on' => true,
+					'warning_code' => '10_SANS_OT',
+					'warning_txt' => "Auto-warning set on VLO6F upload.\nStatuses SGP=A SM=A\n"
+				))
+			));
+		}
+		if( !$no_OT && $row_order['warning_is_on'] && $row_order['warning_code']=='10_SANS_OT' ) {
+			// suppr warning
+			specDbsTracy_order_setWarning( array(
+				'order_filerecord_id' => $row_order['order_filerecord_id'],
+				'data' => json_encode(array(
+					'warning_is_on' => false
+				))
+			));
+		}
+		
+		// Missing attachments ?
+		$missing_attach = ( $statuses['SGP']=='C' && $statuses['SM']=='C' && count($row_order['attachments'])==0 );
+		if( $missing_attach && !$row_order['warning_is_on'] ) {
+			// MeP warning
+			specDbsTracy_order_setWarning( array(
+				'order_filerecord_id' => $row_order['order_filerecord_id'],
+				'data' => json_encode(array(
+					'warning_is_on' => true,
+					'warning_code' => '19_ABS_DOCS',
+					'warning_txt' => "Auto-warning set on VLO6F upload.\nStatuses SGP=C SM=C ATTACH=0\n"
+				))
+			));
+		}
+		if( !$missing_attach && $row_order['warning_is_on'] && $row_order['warning_code']=='19_ABS_DOCS' ) {
+			// suppr warning
+			specDbsTracy_order_setWarning( array(
+				'order_filerecord_id' => $row_order['order_filerecord_id'],
+				'data' => json_encode(array(
+					'warning_is_on' => false
+				))
+			));
+		}
+	}
+	
+	
+	
+	
+	return $passed ;
 }
 
 function specDbsTracy_upload_VL06F_tmp( $handle, $id_soc ) {
