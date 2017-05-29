e1baf2dae0c5d7cf3e30a18ab67b6b332acc1e5d
HP Laptop <dm@mirabel-sil.com>
2017-05-29 18:20:01 +0200

    Spec RSI_RECOUVEO - WIP 28/05 (robot Maileva)

---
+++ resources/server/batchs/RSI_RECOUVEO_outMaileva.php
@@ -0,0 +1,103 @@
+<?php
+session_start() ;
+
+$app_root = dirname($_SERVER['SCRIPT_NAME']).'/../../..' ;
+$server_root=$app_root.'/server' ;
+$resources_root=$app_root.'/resources' ;
+$templates_dir=$resources_root.'/server/templates' ;
+
+@include_once 'PHPExcel/PHPExcel.php' ;
+
+include("$server_root/include/config.inc.php");
+include("$server_root/include/toolfunctions.inc.php");
+include("$server_root/modules/media/include/media.inc.php");
+
+include( "$server_root/include/database/mysql_DB.inc.php" ) ;
+$_opDB = new mysql_DB( );
+$_opDB->connect_mysql( $mysql_host, $mysql_db, $mysql_user, $mysql_pass );
+$_opDB->query("SET NAMES UTF8") ;
+
+//include("$server_root/modules/paracrm/backend_paracrm.inc.php");
+include("$server_root/modules/spec_bp_sales/backend_spec_bp_sales.inc.php");
+
+
+include('RSI_RECOUVEO_outMaileva_mailFactory.inc.php') ;
+
+$_domain_id = DatabaseMgr_Base::dbCurrent_getDomainId() ;
+$_SESSION['login_data']['mysql_db'] = 'op5_'.$_domain_id.'_prod' ;
+$_SESSION['login_data']['login_domain'] = $_domain_id.'_prod' ;
+$_sdomain_id = DatabaseMgr_Sdomain::dbCurrent_getSdomainId() ;
+	
+
+
+
+// *******************************************
+if( TRUE ) {
+	$GLOBALS['maileva_URL'] = 'https://webservices.maileva.com/java/public/connector/ConnectorWebService?wsdl' ;
+	$GLOBALS['maileva_USER'] = 'bluephoenix.partouche' ;
+	$GLOBALS['maileva_PASS'] = 'ss5PjLp' ;
+}
+if( $dev=TRUE ) {
+	$GLOBALS['maileva_URL'] = 'https://webservices.recette.maileva.com/java/public/connector/ConnectorWebService?wsdl' ;
+	$GLOBALS['maileva_USER'] = 'testclient' ;
+	$GLOBALS['maileva_PASS'] = 'testclient' ;
+}
+
+
+$arr_envs = array() ;
+
+// ************ Chargement INV **************
+$query = "SELECT filerecord_id FROM view_file_ENVELOPE WHERE field_TRSPT_STATUS='0' ORDER BY filerecord_id" ;
+$result = $_opDB->query($query) ;
+while( ($arr = $_opDB->fetch_row($result)) != FALSE ) {
+	$arr_envs[] = $arr[0] ;
+}
+if( !$arr_envs ) {
+	exit ;
+}
+
+foreach( $arr_envs as $env_filerecord_id ) {
+		$xml = xml_getContents($env_filerecord_id, $track_email='dm@mirabel-sil.com') ;
+		//echo $xml ;
+		//continue ;
+		
+		$url = $GLOBALS['maileva_URL'] ;
+		$params = array(
+			'soap_version'   => SOAP_1_1,
+			'trace' => false,
+			'login' => $GLOBALS['maileva_USER'],
+			'password' => $GLOBALS['maileva_PASS']
+		) ;
+		$client = new SoapClient($url,$params);
+		
+		$track_id = NULL ;
+		try {
+			$args = new SoapVar($xml, XSD_ANYXML);    
+			
+			$res  = $client->__soapCall('submit', array($args));
+			
+			if( $res ) {
+				$track_id = $res->return ;
+				//var_dump($track_id) ;
+			}
+			echo $env_filerecord_id.':'.$track_id."\n" ;
+		} catch( Exception $e ) {
+			echo "Exception ".$e ;
+			echo "\n\n" ;
+		}
+		
+		//file_put_contents('/var/log/apache2/maileva.xml',$client->__getLastRequest()) ;
+		
+		//die() ;
+		
+		if( $track_id ) {
+			$arr_ins = array() ;
+			$arr_ins['field_TRSPT_STATUS'] = 1 ;
+			$arr_ins['field_TRSPT_CODE'] = 'MAILEVA' ;
+			$arr_ins['field_TRSPT_TRACK'] = $track_id ;
+			paracrm_lib_data_updateRecord_file( 'ENVELOPE' , $arr_ins, $env_filerecord_id ) ;
+		}
+}
+
+exit ;
+?>
---
+++ resources/server/batchs/RSI_RECOUVEO_outMaileva_mailFactory.inc.php
@@ -0,0 +1,160 @@
+<?php
+
+function xmlUtil_parseAdr( $adr_string ) {
+	$adr_string = trim($adr_string) ;
+	$pxml = '' ;
+	
+	$adr_array = array() ;
+	foreach( explode("\n",$adr_string) as $adr_line ) {
+		$adr_line = trim($adr_line) ;
+		if( !$adr_line ) {
+			continue ;
+		}
+		$adr_array[] = $adr_line ;
+	}
+	
+	$pxml.= '<com:AddressLines>' ;
+	for( $i=0 ; $i<count($adr_array)-1 ; $i++ ) {
+		$adr_line = $adr_array[$i] ;
+		$cnt = $i+1 ;
+		$pxml.= "<com:AddressLine{$cnt}>{$adr_line}</com:AddressLine{$cnt}>" ;
+	}
+	$pxml.= "<com:AddressLine6>".end($adr_array)."</com:AddressLine6>" ;
+	$pxml.= '</com:AddressLines>' ;
+	return $pxml ;
+}
+
+function xml_getContents( $env_id, $track_email ) {
+	global $_opDB ;
+	
+	$auth_user = '' ;
+	$auth_pass = '' ;
+
+	$randomId = rand( pow(10,5) , pow(10,6)-1 ) ;
+	
+	$query = "SELECT e.*, ed.filerecord_id as doc_filerecord_id
+		FROM view_file_ENVELOPE e
+		INNER JOIN view_file_ENVELOPE_DOC ed ON ed.filerecord_parent_id = e.filerecord_id
+		WHERE e.filerecord_id='{$env_id}'" ;
+	$result = $_opDB->query($query) ;
+	$db = $_opDB->fetch_assoc($result) ;
+	if( !$db ) {
+		return NULL ;
+	}
+	
+	$adr_string = $db['field_PEER_ADR'] ;
+	
+	
+	media_contextOpen($GLOBALS['_sdomain_id']) ;
+	$inv_pdf = media_pdf_getBinary(media_pdf_toolFile_getId('ENVELOPE_DOC',$db['doc_filerecord_id'])) ;
+	media_contextClose() ;
+	
+	$pdf_pageCount = preg_match_all("/\/Page\W/", $inv_pdf, $dummy);
+	$pdf_pageCount = ( $pdf_pageCount >= 1 ? $pdf_pageCount : 1 ) ;
+	$pdf_size = strlen($inv_pdf) ;
+	$pdf_base64 = base64_encode($inv_pdf) ;
+	//$pdf_base64 = NULL ;
+	
+	$xml = '' ;
+	
+	$xml.= '<con:submit xmlns:con="http://connector.services.siclv2.maileva.fr/">' ;
+	
+	$xml.= '<campaign Version="1.1" TrackId="mon exemple" Application="mailing app" xmlns:com="http://www.maileva.fr/CommonSchema" xmlns:pjs="http://www.maileva.fr/MailevaPJSSchema" xmlns:spec="http://www.maileva.fr/MailevaSpecificSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">' ;
+	
+	/*
+	$xml.= '<pjs:User AuthType="PLAINTEXT">' ;
+	$xml.= '<pjs:Login>testclient</pjs:Login>' ;
+	$xml.= '<pjs:Password>testclient</pjs:Password>' ;
+	$xml.= '</pjs:User>' ;
+	*/
+	
+	$xml.= '<pjs:Requests>' ;
+	$xml.= '<pjs:Request MediaType="PAPER">' ;
+	
+	$xml.= '<pjs:Recipients>' ;
+	$xml.= '<pjs:Internal>' ;
+	$xml.= "<pjs:Recipient Id=\"{$randomId}\">" ;
+	$xml.= '<com:PaperAddress>' ;
+	$xml.= xmlUtil_parseAdr( $db['field_RECEP_ADR'] ) ;
+	$xml.= '</com:PaperAddress>' ;
+	$xml.= '</pjs:Recipient>' ;
+	$xml.= '</pjs:Internal>' ;
+	$xml.= '</pjs:Recipients>' ;
+	
+	$xml.= '<pjs:Senders>' ;
+	$xml.= "<pjs:Sender Id=\"01\">" ;
+	$xml.= '<com:PaperAddress>' ;
+	$xml.= xmlUtil_parseAdr( $db['field_SENDER_ADR'] ) ;
+	$xml.= '</com:PaperAddress>' ;
+	$xml.= '</pjs:Sender>' ;
+	$xml.= '</pjs:Senders>' ;
+	
+	$xml.= '<pjs:DocumentData>' ;
+	$xml.= '<pjs:Documents>' ;
+	$xml.= "<pjs:Document Name=\"flux.pdf\" Id=\"001\">" ;
+	$xml.= "<com:Size>{$pdf_size}</com:Size>" ;
+	$xml.= "<com:Content>" ;
+	$xml.= "<com:Value>{$pdf_base64}</com:Value>" ;
+	$xml.= "</com:Content>" ;
+	$xml.= "</pjs:Document>" ;
+	$xml.= "</pjs:Documents>" ;
+	$xml.= "</pjs:DocumentData>" ;
+	
+	$xml.= '<pjs:Options>' ;
+	$xml.= '<pjs:RequestOption>' ;
+	$xml.= '<spec:PaperOption>' ;
+	$xml.= '<spec:FoldOption>' ;
+	$xml.= '<spec:DocumentOption>' ;
+	$xml.= '<spec:PrintDuplex>1</spec:PrintDuplex>' ;
+	$xml.= '<spec:PageOption>' ;
+	$xml.= '<spec:PrintColor>1</spec:PrintColor>' ;
+	$xml.= '</spec:PageOption>' ;
+	$xml.= '</spec:DocumentOption>' ;
+	$xml.= '<spec:EnvelopeType>C6</spec:EnvelopeType>' ;
+	$xml.= '<spec:PostageClass>STANDARD</spec:PostageClass>' ;
+	$xml.= '<spec:UseFlyLeaf>0</spec:UseFlyLeaf>' ;
+	$xml.= '<spec:PrintSenderAddress>1</spec:PrintSenderAddress>' ;
+	$xml.= '<spec:PrintRecipTrackId>1</spec:PrintRecipTrackId>' ;
+	$xml.= '<spec:DocumentOption>' ;
+	$xml.= '<spec:PrintDuplex>1</spec:PrintDuplex>' ;
+	$xml.= '</spec:DocumentOption>' ;
+	$xml.= '</spec:FoldOption>' ;
+	$xml.= '</spec:PaperOption>' ;
+	$xml.= '</pjs:RequestOption>' ;
+	$xml.= '</pjs:Options>' ;
+	
+	$xml.= '<pjs:Folds>' ;
+	$xml.= "<pjs:Fold Id=\"{$randomId}\" TrackId=\"{$db['field_ENV_REF']}\">" ;
+	$xml.= "<pjs:RecipientId>{$randomId}</pjs:RecipientId>" ;
+	$xml.= "<pjs:SenderId>01</pjs:SenderId>" ;
+	$xml.= '<pjs:Documents>' ;
+	$xml.= "<pjs:Document DocumentId=\"001\" FirstPage=\"1\" LastPage=\"{$pdf_pageCount}\">" ;
+	$xml.= '</pjs:Document>' ;
+	$xml.= '</pjs:Documents>' ;
+	$xml.= '</pjs:Fold>' ;
+	$xml.= '</pjs:Folds>' ;
+	
+	
+	$xml.= '<pjs:Notifications>' ;
+	$xml.= '<pjs:Notification Type="GENERAL">' ;
+	$xml.= '<spec:Format>TXT</spec:Format>' ;
+	$xml.= '<spec:Protocols>' ;
+	$xml.= '<spec:Protocol>' ;
+	$xml.= "<spec:Email>{$track_email}</spec:Email>" ;
+	$xml.= '</spec:Protocol>' ;
+	$xml.= '</spec:Protocols>' ;
+	$xml.= '</pjs:Notification>' ;
+	$xml.= '</pjs:Notifications>' ;
+
+	
+	$xml.= '</pjs:Request>' ;
+	$xml.= '</pjs:Requests>' ;
+	
+	$xml.= '</campaign>' ;
+	
+	$xml.= '</con:submit>' ;
+	
+	return $xml ;
+}
+
+?>
