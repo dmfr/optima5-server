0de32b69601e31da799a104c9a46a7a749712a10
HP Laptop <dm@mirabel-sil.com>
2017-08-07 17:31:13 +0200

    Server - CRM Q-SQL autorun batch, use cfg

--- server/cli/CrmBase_autorun.php
+++ server/cli/CrmBase_autorun.php
@@ -1,4 +1,7 @@
 <?php
+define( 'CRM_AUTORUN_DEFAULT_DELAY_MN', 10 ) ;
+
+
 //ini_set( 'memory_limit', '4096M');
 $server_root = dirname($_SERVER['SCRIPT_NAME']).'/..' ;
 if( $_SERVER['SCRIPT_NAME'] == '' )
@@ -68,26 +71,102 @@ function do_autorun( $domain_id ) {
 function do_autorun_sdomain( $sql_database ) {
 	global $_opDB ;
 	
-	$query = "SELECT sql_querystring, sql_is_rw, qsql_name
+	$query = "SELECT qsql_id, autorun_cfg_json
 		FROM {$sql_database}.qsql
 		WHERE autorun_is_on='O'" ;
 	$result = $_opDB->query($query) ;
 	while( ($arr = $_opDB->fetch_row($result)) != FALSE ) {
-		$sql_querystring = $arr[0] ;
-		$sql_is_rw = ($arr[1]=='O') ;
+		$qsql_id = $arr[0] ;
+		$autorun_cfg = json_decode($arr[1],true) ;
+		if( !$autorun_cfg ) {
+			$autorun_cfg = array(
+				'autorun_is_on' => true,
+				'autorun_mode' => 'repeat',
+				'autorun_repeat_mndelay' => CRM_AUTORUN_DEFAULT_DELAY_MN
+			);
+		}
 		
-		$mt_start = microtime(true) ;
-		$ret = paracrm_queries_qsql_lib_exec($sql_querystring,$sql_is_rw) ;
-		$mt_duration = microtime(true) - $mt_start ;
-		//print_r($ret) ;
+		$query = "SELECT * FROM {$sql_database}.qsql_autorun WHERE qsql_id='{$qsql_id}' ORDER BY qsql_autorun_id DESC LIMIT 1" ;
+		$res = $_opDB->query($query) ;
+		$arr_lastRun = $_opDB->fetch_assoc($res) ;
 		
-		$lig = '' ;
-		$lig = substr_mklig($lig,date('Y-m-d H:i:s'),0,30) ;
-		$lig = substr_mklig($lig,$sql_database,30,30) ;
-		$lig = substr_mklig($lig,$arr[2],60,30) ;
-		$lig = substr_mklig($lig,round($mt_duration,1),90,10) ;
-		echo $lig."\n" ;
+		switch( $autorun_cfg['autorun_mode'] ) {
+			case 'repeat' :
+				$repeat_mndelay = $autorun_cfg['autorun_repeat_mndelay'] ;
+				if( !$arr_lastRun['exec_ts'] ) {
+					do_autorun_qsql($sql_database,$qsql_id) ;
+					break ;
+				}
+				if( ( time() - $arr_lastRun['exec_ts'] ) >= ($repeat_mndelay*60) ) {
+					do_autorun_qsql($sql_database,$qsql_id) ;
+					break ;
+				}
+				break ;
+				
+			case 'schedule' :
+				$day = date('Y-m-d') ;
+				while( TRUE ) {
+					if( strtotime( $day.' '.$autorun_cfg['autorun_schedule_time'] ) > time() ) {
+						$day = date('Y-m-d',strtotime('-1 day',strtotime($day))) ;
+						continue ;
+					}
+					
+					$ts_shouldStart = strtotime( $day.' '.$autorun_cfg['autorun_schedule_time'] ) ;
+					if( !$ts_shouldStart ) {
+						break ;
+					}
+					if( ( !$arr_lastRun['exec_ts'] || ($ts_shouldStart > $arr_lastRun['exec_ts']) ) &&  ($ts_shouldStart > strtotime('-1 hour')) ) {
+						do_autorun_qsql($sql_database,$qsql_id) ;
+					}
+					
+					break ;
+				}
+				break ;
+				
+			default :
+				break ;
+		}
 	}
+}
+function do_autorun_qsql( $sql_database, $qsql_id ) {
+	global $_opDB ;
+	
+	$query = "SELECT sql_querystring, sql_is_rw, qsql_name
+		FROM {$sql_database}.qsql
+		WHERE qsql_id='{$qsql_id}'" ;
+	$result = $_opDB->query($query) ;
+	$arr = $_opDB->fetch_row($result) ;
+	if( !$arr ) {
+		return ;
+	}
+	
+	$sql_querystring = $arr[0] ;
+	$sql_is_rw = ($arr[1]=='O') ;
+	
+	$arr_ins = array() ;
+	$arr_ins['qsql_id'] = $qsql_id ;
+	$arr_ins['exec_ts'] = time() ;
+	$arr_ins['exec_duration'] = 0 ;
+	$_opDB->insert($sql_database.'.'.'qsql_autorun',$arr_ins) ;
+	$qsql_autorun_id = $_opDB->insert_id() ;
+	
+	$mt_start = microtime(true) ;
+	$ret = paracrm_queries_qsql_lib_exec($sql_querystring,$sql_is_rw) ;
+	$mt_duration = microtime(true) - $mt_start ;
+	//print_r($ret) ;
+	
+	$arr_ins = array() ;
+	$arr_ins['exec_duration'] = $mt_duration ;
+	$arr_cond = array() ;
+	$arr_cond['qsql_autorun_id'] = $qsql_autorun_id ;
+	$_opDB->update($sql_database.'.'.'qsql_autorun',$arr_ins,$arr_cond) ;
+	
+	$lig = '' ;
+	$lig = substr_mklig($lig,date('Y-m-d H:i:s'),0,30) ;
+	$lig = substr_mklig($lig,$sql_database,30,30) ;
+	$lig = substr_mklig($lig,$arr[2],60,30) ;
+	$lig = substr_mklig($lig,round($mt_duration,1),90,10) ;
+	echo $lig."\n" ;
 }
 
 if( !getenv('OPTIMA_DB') || count($argv) < 2 ) {
