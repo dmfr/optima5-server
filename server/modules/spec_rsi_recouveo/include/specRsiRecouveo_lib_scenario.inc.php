57c1fc75e33f61357aa7156b2e8293aef0889fea
HP Laptop <dm@mirabel-sil.com>
2018-05-23 13:58:54 +0200

    Spec RSI_RECOUVEO - Cfg scenarios, assoc_is_auto(launch action)

--- server/modules/spec_rsi_recouveo/include/specRsiRecouveo_lib_scenario.inc.php
+++ server/modules/spec_rsi_recouveo/include/specRsiRecouveo_lib_scenario.inc.php
@@ -16,6 +16,25 @@ function specRsiRecouveo_lib_scenario_attach( $reassign=FALSE ) {
 	while( ($arr = $_opDB->fetch_row($result)) != FALSE ) {
 		$file_filerecord_id = $arr[0] ;
 		specRsiRecouveo_lib_scenario_attachFile( $file_filerecord_id ) ;
+		
+		$query = "UPDATE view_file_FILE f SET field_SCENARIO_IS_AUTO='0' WHERE filerecord_id='{$file_filerecord_id}'" ;
+		$_opDB->query($query) ;
+	}
+	
+	
+	
+	$query = "SELECT filerecord_id 
+			FROM view_file_FILE f
+			JOIN view_bible_CFG_STATUS_tree cs ON cs.treenode_key=f.field_STATUS
+			WHERE cs.field_SCHED_LOCK='0' AND cs.field_SCHED_NONE='0' AND f.field_STATUS_CLOSED_VOID<>'1' AND f.field_STATUS_CLOSED_END<>'1'
+			AND f.field_SCENARIO_IS_AUTO='1'" ;
+	$result = $_opDB->query($query) ;
+	while( ($arr = $_opDB->fetch_row($result)) != FALSE ) {
+		$file_filerecord_id = $arr[0] ;
+		specRsiRecouveo_lib_scenario_launchFileIfBump( $file_filerecord_id ) ;
+		
+		$query = "UPDATE view_file_FILE f SET field_SCENARIO_IS_AUTO='0' WHERE filerecord_id='{$file_filerecord_id}'" ;
+		$_opDB->query($query) ;
 	}
 }
 function specRsiRecouveo_lib_scenario_attachFile( $file_filerecord_id ) {
@@ -67,6 +86,58 @@ function specRsiRecouveo_lib_scenario_attachFile( $file_filerecord_id ) {
 		$arr_update = array('field_SCENARIO'=>$assign_scenario) ;
 		paracrm_lib_data_updateRecord_file( 'FILE', $arr_update, $file_record['file_filerecord_id']);
 		
+		if( $file_record['next_action']=='BUMP' ) {
+			$forward_post = array() ;
+			$forward_post['fileaction_filerecord_id'] = $file_record['next_fileaction_filerecord_id'] ;
+			$forward_post['link_status'] = $file_record['status'] ;
+			$forward_post['link_action'] = $file_record['next_action'] ;
+			
+			// next action ?
+			$json = specRsiRecouveo_file_getScenarioLine( array(
+				'file_filerecord_id' => $file_record['file_filerecord_id'],
+				'fileaction_filerecord_id' => $file_record['next_fileaction_filerecord_id']
+			)) ;
+			foreach( $json['data'] as $scenline_dot ) {
+				if( $scenline_dot['is_next'] ) {
+					$forward_post['next_action'] = $scenline_dot['link_action'] ;
+					$forward_post['scen_code'] = $assign_scenario ;
+					$forward_post['next_scenstep_code'] = $scenline_dot['scenstep_code'] ;
+					$forward_post['next_scenstep_tag'] = $scenline_dot['scenstep_tag'] ;
+					$forward_post['next_date'] = $scenline_dot['date_sched'] ;
+				}
+			}
+			
+			$post_data = array(
+				'file_filerecord_id' => $file_record['file_filerecord_id'],
+				'data' => json_encode($forward_post)
+			);
+			specRsiRecouveo_action_doFileAction($post_data) ;
+		}
+	}
+}
+
+
+function specRsiRecouveo_lib_scenario_launchFileIfBump( $file_filerecord_id ) {
+	global $_opDB ;
+	
+	$ttmp = specRsiRecouveo_cfg_getConfig() ;
+	$cfg_atr = $ttmp['data']['cfg_atr'] ;
+
+	
+	/*
+	**********************************************************************
+	- Recherche scenario(S) approprié , ORDER BY min_balance
+	- SI pas de scenario => attrib
+	- SI scenario Actuel > 1er résultat => upgrade (scenario supérieur)
+	- SI scenario Actuel hors cadre => pas de downgrade
+	**************************************************
+	*/
+	$json = specRsiRecouveo_file_getRecords( array(
+		'filter_fileFilerecordId_arr' => json_encode(array($file_filerecord_id))
+	)) ;
+	$file_record = $json['data'][0] ;
+	
+	if( TRUE ) {
 		if( $file_record['next_action']=='BUMP' ) {
 			$forward_post = array() ;
 			$forward_post['fileaction_filerecord_id'] = $file_record['next_fileaction_filerecord_id'] ;
