791549dc23827ef4c5c6ad4effb5baa24d406004
Strange 10.39.1.3 <dev@recouveo-si.com>
2018-07-15 13:03:11 +0200

    Spec RSI_RECOUVEO - Report06/18, ReportFilesPanel Rayane 6/07

--- js/app/Modules/Spec/RsiRecouveo/ReportFilesPanel.js
+++ js/app/Modules/Spec/RsiRecouveo/ReportFilesPanel.js
@@ -8,6 +8,7 @@ Ext.define('Optima5.Modules.Spec.RsiRecouveo.ReportFilesPanel', {
 	],
 	_ajaxData: null,
 	_filesWidgetList : null,
+	_listCnt: null,
 	initComponent: function () {
 		Ext.apply(this,{
 			_hideDates: true,
@@ -52,7 +53,7 @@ Ext.define('Optima5.Modules.Spec.RsiRecouveo.ReportFilesPanel', {
 						scope: this
 					},
 
-					title: 'Agenda (nb dossiers)',
+					title: 'Agenda: Nombre de dossiers',
 					_defaultMode: 'count',
 					_dashboardMode: true,
 					_hideForm: true,
@@ -64,7 +65,7 @@ Ext.define('Optima5.Modules.Spec.RsiRecouveo.ReportFilesPanel', {
 				},Ext.create('Optima5.Modules.Spec.RsiRecouveo.FilesWidgetAgenda', {
 					flex: 1,
 					itemId: 'centerWidgetAgendaAmount',
-					title: 'Agenda (devise €)',
+					title: 'Agenda: Devise en euro',
 					_defaultMode: 'amount',
 					_hideForm: true,
 					listeners: {
@@ -135,6 +136,19 @@ Ext.define('Optima5.Modules.Spec.RsiRecouveo.ReportFilesPanel', {
 		this.down('#centerWidgetAgendaCount').loadFilesData( ajaxData ) ;
 		this.down('#centerWidgetAgendaAmount').loadFilesData( ajaxData ) ;
 	},
+	handleOpenAccount: function(accId,fileFilerecordId) {
+		var objAtrFilter = {} ;
+		Ext.Array.each( this.query('toolbar > [cfgParam_id]'), function(cfgParamBtn) {
+			objAtrFilter[cfgParamBtn.cfgParam_id] = cfgParamBtn.getValue()
+		}) ;
+
+		this.optimaModule.postCrmEvent('openaccount',{
+			accId:accId,
+			filterAtr:objAtrFilter,
+			focusFileFilerecordId:fileFilerecordId,
+			showClosed: this.showClosed
+		}) ;
+	},
 	showLoadmask: function() {
 		if( this.rendered ) {
 			this.doShowLoadmask() ;
@@ -158,7 +172,6 @@ Ext.define('Optima5.Modules.Spec.RsiRecouveo.ReportFilesPanel', {
 			this.loadMask = null ;
 		}
 	},
-
 	onAgendaItemClick: function( clickAgendaClass, clickEtaRange) {
 		// Filtre par sociétés => params à affficher
 		var tbSocsSelected ;
@@ -189,28 +202,59 @@ Ext.define('Optima5.Modules.Spec.RsiRecouveo.ReportFilesPanel', {
 		var curWidth = this.getEl().getWidth(),
 			curHeight = this.getEl().getHeight() ;
 		var filesWidgetList = Ext.create('Optima5.Modules.Spec.RsiRecouveo.FilesWidgetList', {
-				//itemId: 'pGrid',
-				height:(curHeight*0.8),
-				width:(curWidth*0.8),
-				floating: true,
-				draggable: true,
-				resizable: true,
-				renderTo: this.getEl(),
-				constrain: true,
-				closable: true,
-				frame: true,
-				title: 'Files list'
+			itemId: 'pList',
+			listeners: {
+				openaccount: this.handleOpenAccount,
+				multiselect: this.onGridMultiSelect,
+				scope: this,
+			},
+			scope: this,
+			optimaModule: this.optimaModule
 		}) ;
-		filesWidgetList.on('destroy', function(p){
+		var listCnt = Ext.create('Ext.panel.Panel', {
+			height:(curHeight*0.8),
+			width:(curWidth*0.8),
+			floating: true,
+			draggable: true,
+			resizable: true,
+			renderTo: this.getEl(),
+			constrain: true,
+			closable: true,
+			frame: true,
+			scrollable: true,
+			title: 'Liste des dossiers',
+			layout: 'fit',
+			tbar: [{
+				iconCls: 'op5-spec-rsiveo-datatoolbar-new',
+				text: 'Select.multiple',
+				handler: function(){
+					this._filesWidgetList.toggleMultiSelect() ;
+				},
+				scope: this
+			},{
+				iconCls: 'op5-spec-rsiveo-datatoolbar-file-export-excel',
+				text: 'Export',
+				handler: function() {
+					this.handleDownload() ;
+				},
+				scope: this
+			}],
+			items: [filesWidgetList]
+		}) ;
+		listCnt.down('#pList').on('destroy', function(p){
 			this.getEl().unmask() ;
 			this._filesWidgetList = null ;
 		},this,{single:true}) ;
-		filesWidgetList.configureGrid(cfgParamIds, false, 'file') ;
-		filesWidgetList.loadFilesData(this._ajaxData, true) ;
-		filesWidgetList.getEl().alignTo(this.getEl(), 'c-c?');
-		filesWidgetList.show();
+		listCnt.on('destroy', function(p){
+			this.getEl().unmask() ;
+			this.listCnt = null ;
+		},this,{single:true}) ;
+		listCnt.down('#pList').configureGrid(cfgParamIds, false, 'file') ;
+		listCnt.down('#pList').loadFilesData(this._ajaxData, true) ;
+		listCnt.down('#pList').getEl().alignTo(this.getEl(), 'c-c?');
+		listCnt.show();
 		this._filesWidgetList = filesWidgetList ;
-		
+		this._listCnt = listCnt;
 		
 		
 		// Filtres : TODO: déplacer dans FilesWidgetList
@@ -261,12 +305,141 @@ Ext.define('Optima5.Modules.Spec.RsiRecouveo.ReportFilesPanel', {
 		}
 
 	},
-	
-	
+
+	handleDownload: function() {
+		var mapFieldString = {} ;
+		Ext.Array.each( this._filesWidgetList.getStore().getModel().getFields(), function(field) {
+			mapFieldString[field.getName()] = Ext.Array.contains(['string'],field.getType()) ;
+		}) ;
+
+		var columns = [] ;
+
+		Ext.Array.each( this._filesWidgetList.headerCt.getGridColumns(), function(column) {
+			if( !column.isVisible(true) ) {
+				return ;
+			}
+			columns.push({
+				dataIndex: column.dataIndex,
+				dataIndexString: mapFieldString[column.dataIndex],
+				text: column.text
+			});
+		});
+
+		var data = [] ;
+		this._filesWidgetList.getStore().each( function(record) {
+			var recData = record.getData(true) ;
+			delete recData['actions'] ;
+			delete recData['inv_balage'] ;
+			delete recData['records'] ;
+			data.push( recData ) ;
+		}) ;
+
+		var exportParams = this.optimaModule.getConfiguredAjaxParams() ;
+		Ext.apply(exportParams,{
+			_moduleId: 'spec_rsi_recouveo',
+			_action: 'xls_create',
+			columns: Ext.JSON.encode(columns),
+			data: Ext.JSON.encode(data),
+			exportXls: true
+		}) ;
+		Ext.create('Ext.ux.dams.FileDownloader',{
+			renderTo: Ext.getBody(),
+			requestParams: exportParams,
+			requestAction: Optima5.Helper.getApplication().desktopGetBackendUrl(),
+			requestMethod: 'POST'
+		}) ;
+	},
 	
 	onBeforeDestroy: function() {
 		if( this._filesWidgetList ) {
 			this._filesWidgetList.destroy() ;
 		}
-	}
+		if (this._listCnt){
+			this._listCnt.onDestroy() ;
+		}
+	},
+	onGridMultiSelect: function( widgetList, ids ) {
+		this.handleMultiSelect(ids) ;
+	},
+	handleMultiSelect: function(ids) {
+		this.getEl().mask() ;
+		// Open panel
+		var createPanel = Ext.create('Optima5.Modules.Spec.RsiRecouveo.MultiActionForm',{
+			_arr_fileFilerecordIds: ids,
+			optimaModule: this.optimaModule,
+			width:400, // dummy initial size, for border layout to work
+			height:null, // ...
+			floating: true,
+			draggable: true,
+			resizable: true,
+			renderTo: this.getEl(),
+			tools: [{
+				type: 'close',
+				handler: function(e, t, p) {
+					p.ownerCt.destroy();
+				},
+				scope: this
+			}],
+
+			listeners: {
+				btnsubmit: this.onMultiSelectSubmit,
+				scope: this
+			}
+		});
+		createPanel.on('saved', function(p) {
+			this.doTreeLoad() ;
+		},this,{single:true}) ;
+		createPanel.on('destroy',function(p) {
+			this.getEl().unmask() ;
+			this.multiActionForm = null ;
+		},this,{single:true}) ;
+
+		createPanel.show();
+		createPanel.getEl().alignTo(this.getEl(), 'c-c?');
+
+		this.multiActionForm = createPanel ;
+	},
+	onMultiSelectSubmit: function(p,formValues) {
+		var ids = p._arr_fileFilerecordIds ;
+		if( Ext.isEmpty(ids) ) {
+			return ;
+		}
+
+		var gridPanel = this._filesWidgetList,
+			gridPanelStore = gridPanel.getStore() ;
+		gridPanelStore.each( function(r) {
+			if( r.get('_is_selection') && !Ext.Array.contains(ids,r.get('file_filerecord_id')) ) {
+				ids.push( r.get('file_filerecord_id') ) ;
+			}
+		}) ;
+
+
+		if( this.multiActionForm ) {
+			this.multiActionForm.mask('Modifications en cours...') ;
+		}
+		this.optimaModule.getConfiguredAjaxConnection().request({
+			params: {
+				_moduleId: 'spec_rsi_recouveo',
+				_action: 'file_multiAction',
+
+				select_fileFilerecordIds: Ext.JSON.encode(ids),
+				target_form: Ext.JSON.encode(formValues)
+			},
+			success: function(response) {
+				var ajaxResponse = Ext.decode(response.responseText) ;
+				if( ajaxResponse.success == false ) {
+					Ext.MessageBox.alert('Error','Error') ;
+					return ;
+				}
+				this._filesWidgetList.loadFilesData(this._ajaxData, false) ;
+				//this.doLoad(false) ;
+			},
+			callback: function() {
+				if( this.multiActionForm ) {
+					this.multiActionForm.destroy() ;
+				}
+			},
+			scope: this
+		}) ;
+	},
 })
