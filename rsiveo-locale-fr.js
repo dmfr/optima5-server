7a7f8e23970efe0f1ba8bbb161cfafe7ff829463
Strange 10.39.1.3 <dev@recouveo-si.com>
2018-07-15 13:03:44 +0200

    Spec RSI_RECOUVEO - Rayane 07/2018: FR numberformat

--- rsiveo-locale-fr.js
+++ rsiveo-locale-fr.js
@@ -3,8 +3,18 @@
  * By Rayane
  * for Recouveo SI
  */
-
-Ext.define("Ext.locale.en.grid.filters.filter.Date", {
+Ext.onReady(function() {
+	if (Ext.util && Ext.util.Format) {
+		Ext.apply(Ext.util.Format, {
+			thousandSeparator: ' ',
+			decimalSeparator: ',',
+			currencySign: '\u20ac',
+			// French Euro
+			dateFormat: 'd/m/Y'
+		});
+	}
+});
+Ext.define("Ext.locale.fr.grid.filters.filter.Date", {
     override: "Ext.grid.filters.filter.Date",
     config: {
         fields: {
@@ -17,22 +27,28 @@ Ext.define("Ext.locale.en.grid.filters.filter.Date", {
     dateFormat: null
 });
 
-Ext.define("Ext.locale.en.form.Basic", {
+Ext.define("Ext.locale.fr.form.Basic", {
     override: "Ext.form.Basic",
     waitTitle: "Chargement..."
 });
 
-Ext.define("Ext.locale.en.grid.filters.filter.List", {
+Ext.define("Ext.locale.fr.util.Format", {
+	override: "Ext.util.Format",
+	thousandSeparator: ' ',
+	decimalSeparator: ','
+});
+
+Ext.define("Ext.locale.fr.grid.filters.filter.List", {
     override: "Ext.grid.filters.filter.List",
     loadingText: "Chargement..."
 });
 
-Ext.define("Ext.locale.en.grid.filters.filter.Number", {
+Ext.define("Ext.locale.fr.grid.filters.filter.Number", {
     override: "Ext.grid.filters.filter.Number",
     emptyText: "Entrez un nombre..."
 });
 
-Ext.define("Ext.locale.en.grid.filters.filter.String", {
+Ext.define("Ext.locale.fr.grid.filters.filter.String", {
     override: "Ext.grid.filters.filter.String",
     emptyText: "Entrez le texte..."
 });
