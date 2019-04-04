30902cb58cd2dee74032cb6a8fdd3914269ba735
HP Laptop <dm@mirabel-sil.com>
2019-04-04 14:55:34 +0200

    Resources - JP_SALES_invMaileva (test 03/04/19)

---
+++ resources/server/batchs/JP_SALES_invMaileva.php
@@ -0,0 +1,152 @@
+<?php
+session_start() ;
+
+$app_root = dirname($_SERVER['SCRIPT_NAME']).'/../../..' ;
+$server_root=$app_root.'/server' ;
+$resources_root=$app_root.'/resources' ;
+$templates_dir=$resources_root.'/server/templates' ;
+
+@include_once 'PHPExcel/PHPExcel.php' ;
+
+include("$server_root/include/config.inc.php");
+include("$server_root/include/toolfunctions.inc.php");
+include("$server_root/modules/media/include/media.inc.php");
+
+include( "$server_root/include/database/mysql_DB.inc.php" ) ;
+$_opDB = new mysql_DB( );
+$_opDB->connect_mysql( $mysql_host, $mysql_db, $mysql_user, $mysql_pass );
+$_opDB->query("SET NAMES UTF8") ;
+
+//include("$server_root/modules/paracrm/backend_paracrm.inc.php");
+include("$server_root/modules/spec_bp_sales/backend_spec_bp_sales.inc.php");
+
+
+include('JP_SALES_invMaileva_mailFactory.inc.php') ;
+include('JP_SALES_invEurofactor_mailFactory.inc.php') ;
+
+
+
+// *********** Get CONSTANTES *******************
+$GLOBALS['factor_ref'] = date('ymdHis') ;
+
+$GLOBALS['_cfg_peer_code'] = 'INV_MAILEVA' ;
+$GLOBALS['_cfg_peer_code_parent'] = 'INV_EUROFACTOR' ;
+
+$query = "SELECT field_MAIL_SENDTO, field_PARAMS FROM view_bible_CFG_PEER_entry
+	WHERE entry_key='{$GLOBALS['_cfg_peer_code']}'" ;
+$result = $_opDB->query($query) ;
+if( $_opDB->num_rows($result) != 1 ) {
+	die() ;
+}
+$arr = $_opDB->fetch_row($result) ;
+
+$field_params = $arr[1] ;
+$field_sendto = $arr[0] ;
+
+$GLOBALS['arr_mailinglist'] = array() ;
+foreach( explode(',',$field_sendto) as $email ) {
+	$GLOBALS['arr_mailinglist'][] = trim($email) ;
+}
+
+foreach( explode(',',$field_params) as $param ) {
+	$ttmp = explode('=',$param) ;
+	if( count($ttmp) != 2 ) {
+		continue ;
+	}
+	$mkey = trim($ttmp[0]) ;
+	$mval = trim($ttmp[1]) ;
+	switch( $mkey ) {
+		case 'URL' :
+			$GLOBALS['maileva_URL'] = $mval ;
+			break ;
+		case 'USER' :
+			$GLOBALS['maileva_USER'] = $mval ;
+			break ;
+		case 'PASS' :
+			$GLOBALS['maileva_PASS'] = $mval ;
+			break ;
+		default :
+			break ;
+	}
+}
+// *******************************************
+if( $GLOBALS['__OPTIMA_TEST'] ) {
+	$GLOBALS['maileva_URL'] = 'https://webservices.recette.maileva.com/java/public/connector/ConnectorWebService?wsdl' ;
+	$GLOBALS['maileva_USER'] = 'testclient' ;
+	$GLOBALS['maileva_PASS'] = 'testclient' ;
+}
+
+
+
+$map_factorRef_invFilerecordIds = array() ;
+
+// ************ Chargement INV **************
+$query = "SELECT ipp.field_SEND_REF, i.filerecord_id FROM view_file_INV i
+	LEFT OUTER JOIN view_file_INV_PEER ip ON ip.filerecord_parent_id=i.filerecord_id
+		AND ip.field_PEER_CODE='{$GLOBALS['_cfg_peer_code']}'
+	LEFT OUTER JOIN view_file_INV_PEER ipp ON ipp.filerecord_parent_id=i.filerecord_id
+		AND ipp.field_PEER_CODE='{$GLOBALS['_cfg_peer_code_parent']}'
+	WHERE i.field_STATUS_IS_FINAL='1'
+	AND (ip.field_SEND_IS_OK IS NULL OR ip.field_SEND_IS_OK<>'1')
+	AND (ipp.field_SEND_IS_OK='1')
+	AND ABS(i.field_CALC_AMOUNT_FINAL) > '0'" ;
+$result = $_opDB->query($query) ;
+while( ($arr = $_opDB->fetch_row($result)) != FALSE ) {
+	$factor_SEND_REF = $arr[0] ;
+	$filerecord_id = $arr[1] ;
+	if( !$map_factorRef_invFilerecordIds ) {
+		$map_factorRef_invFilerecordIds[$factor_SEND_REF] = array() ;
+	}
+	$map_factorRef_invFilerecordIds[$factor_SEND_REF][] = $filerecord_id ;
+}
+if( !$map_factorRef_invFilerecordIds ) {
+	exit ;
+}
+
+foreach( $map_factorRef_invFilerecordIds as $factor_SEND_REF => $arr_invFilerecordIds ) {
+	foreach( $arr_invFilerecordIds as $inv_filerecord_id ) {
+		$xml = xml_getContents($inv_filerecord_id, $track_email=$field_sendto) ;
+		//echo $xml ;
+		
+		$url = $GLOBALS['maileva_URL'] ;
+		$params = array(
+			'soap_version'   => SOAP_1_1,
+			'trace' => false,
+			'login' => $GLOBALS['maileva_USER'],
+			'password' => $GLOBALS['maileva_PASS']
+		) ;
+		$client = new SoapClient($url,$params);
+		
+		$track_id = NULL ;
+		try {
+			$args = new SoapVar($xml, XSD_ANYXML);    
+			
+			$res  = $client->__soapCall('submit', array($args));
+			
+			if( $res ) {
+				$track_id = $res->return ;
+				//var_dump($track_id) ;
+			}
+			echo $inv_filerecord_id.':'.$track_id."\n" ;
+		} catch( Exception $e ) {
+			echo "Exception ".$e ;
+			echo "\n\n" ;
+		}
+		
+		//file_put_contents('/var/log/apache2/maileva.xml',$client->__getLastRequest()) ;
+		
+		//die() ;
+		
+		if( $track_id ) {
+			$arr_ins = array() ;
+			$arr_ins['field_PEER_CODE'] = $GLOBALS['_cfg_peer_code'] ;
+			$arr_ins['field_SEND_IS_OK'] = 1 ;
+			$arr_ins['field_SEND_REF'] = $track_id ;
+			$arr_ins['field_SEND_DATE'] = date('Y-m-d H:i:s') ;
+			paracrm_lib_data_insertRecord_file( 'INV_PEER' , $inv_filerecord_id , $arr_ins ) ;
+		}
+	}
+}
+
+exit ;
+?>
---
+++ resources/server/batchs/JP_SALES_invMaileva_mailFactory.inc.php
@@ -0,0 +1,158 @@
+<?php
+
+function xmlUtil_parseAdr( $adr_string ) {
+	$adr_string = trim($adr_string) ;
+	$pxml = '' ;
+	
+	$adr_array = array() ;
+	foreach( explode("\n",$adr_string) as $adr_line ) {
+		$adr_line = trim($adr_line) ;
+		if( !$adr_line ) {
+			continue ;
+		}
+		$adr_array[] = $adr_line ;
+	}
+	
+	$pxml.= '<com:AddressLines>' ;
+	for( $i=0 ; $i<count($adr_array)-1 ; $i++ ) {
+		$adr_line = $adr_array[$i] ;
+		$cnt = $i+1 ;
+		$pxml.= "<com:AddressLine{$cnt}>{$adr_line}</com:AddressLine{$cnt}>" ;
+	}
+	$pxml.= "<com:AddressLine6>".end($adr_array)."</com:AddressLine6>" ;
+	$pxml.= '</com:AddressLines>' ;
+	return $pxml ;
+}
+
+function xml_getContents( $inv_filerecord_id, $track_email ) {
+	global $_opDB ;
+	
+	$track_id = 'JP-INV-'.$inv_filerecord_id ;
+	
+	$auth_user = '' ;
+	$auth_pass = '' ;
+
+	$randomId = rand( pow(10,5) , pow(10,6)-1 ) ;
+	
+	$query = "SELECT i.*
+		FROM view_file_INV i
+		INNER JOIN view_bible_CUSTOMER_entry c ON c.entry_key = i.field_CLI_LINK
+		WHERE filerecord_id='{$inv_filerecord_id}'" ;
+	$result = $_opDB->query($query) ;
+	$arr = $_opDB->fetch_assoc($result) ;
+	if( !$arr ) {
+		return NULL ;
+	}
+	
+	if( trim($arr['field_ADR_SENDTO']) ) {
+		$adr_string = $arr['field_ADR_SENDTO'] ;
+	} elseif( trim($arr['field_ADR_INVOICE']) ) {
+		$adr_string = $arr['field_ADR_INVOICE'] ;
+	} else {
+		$adr_string = $arr['field_ADR_SHIP'] ;
+	}
+	
+	$json = specBpSales_inv_printDoc( array('inv_filerecord_id'=>$inv_filerecord_id) ) ;
+	if( !$json['success'] ) {
+		return NULL ;
+	}
+	$inv_html = $json['html'] ;
+	$inv_pdf = specBpSales_util_htmlToPdf_buffer( $inv_html ) ;
+	$pdf_pageCount = preg_match_all("/\/Page\W/", $inv_pdf, $dummy);
+	$pdf_pageCount = ( $pdf_pageCount >= 1 ? $pdf_pageCount : 1 ) ;
+	$pdf_size = strlen($inv_pdf) ;
+	$pdf_base64 = base64_encode($inv_pdf) ;
+	//$pdf_base64 = NULL ;
+	
+	$xml = '' ;
+	
+	$xml.= '<con:submit xmlns:con="http://connector.services.siclv2.maileva.fr/">' ;
+	
+	$xml.= '<campaign Version="1.1" TrackId="mon exemple" Application="mailing app" xmlns:com="http://www.maileva.fr/CommonSchema" xmlns:pjs="http://www.maileva.fr/MailevaPJSSchema" xmlns:spec="http://www.maileva.fr/MailevaSpecificSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">' ;
+	
+	/*
+	$xml.= '<pjs:User AuthType="PLAINTEXT">' ;
+	$xml.= '<pjs:Login>testclient</pjs:Login>' ;
+	$xml.= '<pjs:Password>testclient</pjs:Password>' ;
+	$xml.= '</pjs:User>' ;
+	*/
+	
+	$xml.= '<pjs:Requests>' ;
+	$xml.= "<pjs:Request MediaType=\"PAPER\" TrackId=\"{$track_id}\">" ;
+	
+	$xml.= '<pjs:Recipients>' ;
+	$xml.= '<pjs:Internal>' ;
+	$xml.= "<pjs:Recipient Id=\"{$randomId}\">" ;
+	$xml.= '<com:PaperAddress>' ;
+	$xml.= xmlUtil_parseAdr( $adr_string ) ;
+	$xml.= '</com:PaperAddress>' ;
+	$xml.= '</pjs:Recipient>' ;
+	$xml.= '</pjs:Internal>' ;
+	$xml.= '</pjs:Recipients>' ;
+	
+	$xml.= '<pjs:DocumentData>' ;
+	$xml.= '<pjs:Documents>' ;
+	$xml.= "<pjs:Document Name=\"flux.pdf\" Id=\"001\">" ;
+	$xml.= "<com:Size>{$pdf_size}</com:Size>" ;
+	$xml.= "<com:Content>" ;
+	$xml.= "<com:Value>{$pdf_base64}</com:Value>" ;
+	$xml.= "</com:Content>" ;
+	$xml.= "</pjs:Document>" ;
+	$xml.= "</pjs:Documents>" ;
+	$xml.= "</pjs:DocumentData>" ;
+	
+	$xml.= '<pjs:Options>' ;
+	$xml.= '<pjs:RequestOption>' ;
+	$xml.= '<spec:PaperOption>' ;
+	$xml.= '<spec:FoldOption>' ;
+	$xml.= '<spec:DocumentOption>' ;
+	$xml.= '<spec:PrintDuplex>1</spec:PrintDuplex>' ;
+	$xml.= '<spec:PageOption>' ;
+	$xml.= '<spec:PrintColor>1</spec:PrintColor>' ;
+	$xml.= '</spec:PageOption>' ;
+	$xml.= '</spec:DocumentOption>' ;
+	$xml.= '<spec:EnvelopeType>C6</spec:EnvelopeType>' ;
+	$xml.= '<spec:PostageClass>STANDARD</spec:PostageClass>' ;
+	$xml.= '<spec:UseFlyLeaf>1</spec:UseFlyLeaf>' ;
+	$xml.= '<spec:DocumentOption>' ;
+	$xml.= '<spec:PrintDuplex>0</spec:PrintDuplex>' ;
+	$xml.= '</spec:DocumentOption>' ;
+	$xml.= '</spec:FoldOption>' ;
+	$xml.= '</spec:PaperOption>' ;
+	$xml.= '</pjs:RequestOption>' ;
+	$xml.= '</pjs:Options>' ;
+	
+	$xml.= '<pjs:Folds>' ;
+	$xml.= "<pjs:Fold Id=\"{$randomId}\">" ;
+	$xml.= "<pjs:RecipientId>{$randomId}</pjs:RecipientId>" ;
+	$xml.= '<pjs:Documents>' ;
+	$xml.= "<pjs:Document DocumentId=\"001\" FirstPage=\"1\" LastPage=\"{$pdf_pageCount}\">" ;
+	$xml.= '</pjs:Document>' ;
+	$xml.= '</pjs:Documents>' ;
+	$xml.= '</pjs:Fold>' ;
+	$xml.= '</pjs:Folds>' ;
+	
+	
+	$xml.= '<pjs:Notifications>' ;
+	$xml.= '<pjs:Notification Type="GENERAL">' ;
+	$xml.= '<spec:Format>TXT</spec:Format>' ;
+	$xml.= '<spec:Protocols>' ;
+	$xml.= '<spec:Protocol>' ;
+	$xml.= "<spec:Email>{$track_email}</spec:Email>" ;
+	$xml.= '</spec:Protocol>' ;
+	$xml.= '</spec:Protocols>' ;
+	$xml.= '</pjs:Notification>' ;
+	$xml.= '</pjs:Notifications>' ;
+
+	
+	$xml.= '</pjs:Request>' ;
+	$xml.= '</pjs:Requests>' ;
+	
+	$xml.= '</campaign>' ;
+	
+	$xml.= '</con:submit>' ;
+	
+	return $xml ;
+}
+
+?>
