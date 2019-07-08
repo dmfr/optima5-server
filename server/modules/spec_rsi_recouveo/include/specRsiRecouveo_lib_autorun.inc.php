9426ebade6b48c2a206b7190e01918e3d3173ef5
Strange 10.39.1.3 <strange@mirabel-sil.com>
2019-07-08 15:23:32 +0200

    Spec RSI_RECOUVEO - Autorun, closeEnd reopen

--- server/modules/spec_rsi_recouveo/include/specRsiRecouveo_lib_autorun.inc.php
+++ server/modules/spec_rsi_recouveo/include/specRsiRecouveo_lib_autorun.inc.php
@@ -121,7 +121,14 @@ function specRsiRecouveo_lib_autorun_manageActivate() {
 
 function specRsiRecouveo_lib_autorun_closeEnd() {
 	global $_opDB ;
-	$query = "SELECT distinct field_LINK_ACCOUNT FROM view_file_FILE WHERE field_STATUS_CLOSED_END<>'1' AND field_STATUS_CLOSED_VOID<>'1'" ;
+	$query = "SELECT distinct field_LINK_ACCOUNT 
+					FROM view_file_FILE 
+					WHERE (field_STATUS_CLOSED_END<>'1' AND field_STATUS_CLOSED_VOID<>'1') 
+					OR field_LINK_ACCOUNT IN (
+						select distinct field_LINK_ACCOUNT 
+						FROM view_file_RECORD 
+						WHERE field_LETTER_IS_CONFIRM<>'1'
+					)" ;
 	$result = $_opDB->query($query) ;
 	while( ($arr = $_opDB->fetch_row($result)) != FALSE ) {
 		$acc_id = $arr[0] ;
