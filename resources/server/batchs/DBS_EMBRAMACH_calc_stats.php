f64624d5b33c0a22cd52052047be876e67a743b3
Centos8 10.39.10.205 <proxmox@mirabel-sil.com>
2020-05-12 12:54:58 +0200

    Spec DBS_EMBRAMACH - Picking KPI, man calc batch

---
+++ resources/server/batchs/DBS_EMBRAMACH_calc_stats.php
@@ -0,0 +1,28 @@
+<?php
+$app_root = dirname($_SERVER['SCRIPT_NAME']).'/../../..' ;
+$server_root=$app_root.'/server' ;
+$resources_root=$app_root.'/resources' ;
+$templates_dir=$resources_root.'/server/templates' ;
+
+include("$server_root/include/config.inc.php");
+include("$server_root/include/toolfunctions.inc.php");
+include("$server_root/modules/media/include/media.inc.php");
+
+include( "$server_root/include/database/mysql_DB.inc.php" ) ;
+$_opDB = new mysql_DB( );
+$_opDB->connect_mysql( $mysql_host, $mysql_db, $mysql_user, $mysql_pass );
+$_opDB->query("SET NAMES UTF8") ;
+
+include("$server_root/modules/spec_dbs_embramach/backend_spec_dbs_embramach.inc.php");
+
+//ini_set( 'memory_limit', '4096M');
+
+
+$file_code = 'FLOW_PICKING' ;
+$file_code_step = 'FLOW_PICKING_STEP' ;
+
+
+specDbsEmbramach_stats_sub_prepareData() ;
+
+
+?>
