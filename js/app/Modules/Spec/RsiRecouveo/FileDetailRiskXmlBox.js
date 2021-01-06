9f0f56d64bc71a487db86542a22e7f95766bb960
Centos8 10.39.10.205 <proxmox@mirabel-sil.com>
2021-01-06 14:44:52 +0100

    Spec RSI_RECOUVEO - FileDetailRiskXmlBox, code clean

--- js/app/Modules/Spec/RsiRecouveo/FileDetailRiskXmlBox.js
+++ js/app/Modules/Spec/RsiRecouveo/FileDetailRiskXmlBox.js
@@ -71,119 +71,108 @@ Ext.define('Optima5.Modules.Spec.RsiRecouveo.FileDetailRiskXmlBox', {
 		return xml;
 	},
 	buildXmlViewNode: function( parent, xml ) {
-        if (xml.nodeType == 9) {
-            for (var i = 0 ; i < xml.childNodes.length ; i++)
-                this.buildXmlViewNode(parent, xml.childNodes[i]);
+		if (xml.nodeType == 9) {
+			for (var i = 0 ; i < xml.childNodes.length ; i++) {
+				this.buildXmlViewNode(parent, xml.childNodes[i]);
+			}
+			return;
+		}
+		
+		switch (xml.nodeType) {
+			case 1: // Simple element
+				var hasChildNodes = xml.childNodes.length > 0;
+				var expandingNode = hasChildNodes && (xml.childNodes.length > 1 || xml.childNodes[0].nodeType != 3);
 
-            return;
-        }
+				var expanderHeader = expandingNode ? makeSpan("", "op5-spec-rsiveo-xmlbox-expanderHeader") : parent;
 
-        switch (xml.nodeType) {
-            case 1: // Simple element
-                {
-                    var hasChildNodes = xml.childNodes.length > 0;
-                    var expandingNode = hasChildNodes && (xml.childNodes.length > 1 || xml.childNodes[0].nodeType != 3);
+				expanderHeader.appendChild(makeSpan("<", "op5-spec-rsiveo-xmlbox-tagHeader"));
+				expanderHeader.appendChild(makeSpan(xml.nodeName, "op5-spec-rsiveo-xmlbox-tagValue"));
 
-                    var expanderHeader = expandingNode ? makeSpan("", "op5-spec-rsiveo-xmlbox-expanderHeader") : parent;
+				if( expandingNode) {
+					parent.appendChild(expanderHeader);
+				}
 
-                    expanderHeader.appendChild(makeSpan("<", "op5-spec-rsiveo-xmlbox-tagHeader"));
-                    expanderHeader.appendChild(makeSpan(xml.nodeName, "op5-spec-rsiveo-xmlbox-tagValue"));
+				// Handle attributes
+				var attributes = xml.attributes;
+				for( var attrIdx = 0 ; attrIdx < attributes.length ; attrIdx++ ) {
+					expanderHeader.appendChild( makeSpan( " "));
+					expanderHeader.appendChild( makeSpan( attributes [attrIdx].name, "op5-spec-rsiveo-xmlbox-attrName"));
+					expanderHeader.appendChild( makeSpan( '="' ));
+					expanderHeader.appendChild( makeSpan( attributes [attrIdx].value, "op5-spec-rsiveo-xmlbox-attrValue" ));
+					expanderHeader.appendChild( makeSpan( '"' ));
+				}
 
-                    if( expandingNode)
-                        parent.appendChild(expanderHeader);
+				// Handle child nodes
+				if (hasChildNodes) {
+					parent.appendChild(makeSpan(">", "op5-spec-rsiveo-xmlbox-tagHeader"));
 
-                    // Handle attributes
-					var attributes = xml.attributes;
-					for( var attrIdx = 0 ; attrIdx < attributes.length ; attrIdx++ ) {
-						expanderHeader.appendChild( makeSpan( " "));
-						expanderHeader.appendChild( makeSpan( attributes [attrIdx].name, "op5-spec-rsiveo-xmlbox-attrName"));
-						expanderHeader.appendChild( makeSpan( '="' ));
-						expanderHeader.appendChild( makeSpan( attributes [attrIdx].value, "op5-spec-rsiveo-xmlbox-attrValue" ));
-						expanderHeader.appendChild( makeSpan( '"' ));
+					if (expandingNode) {
+						var ulElement = document.createElement("ul");
+						for (var i = 0 ; i < xml.childNodes.length ; i++) {
+							var liElement = document.createElement("li");
+							this.buildXmlViewNode(liElement, xml.childNodes[i]);
+							ulElement.appendChild(liElement);
+						}
+
+						ulElement.setAttribute("class", "op5-spec-rsiveo-xmlbox-content");
+						//parent.appendChild(collapsedTextSpan);
+						parent.appendChild(ulElement);
+
+						parent.appendChild(makeSpan("", "op5-spec-rsiveo-xmlbox-expanderClose"));
+					} else {
+						parent.appendChild(makeSpan(xml.childNodes[0].nodeValue));
 					}
-					
-                    // Handle child nodes
-                    if (hasChildNodes) {
 
-                        parent.appendChild(makeSpan(">", "op5-spec-rsiveo-xmlbox-tagHeader"));
+					// Closing tag
+					parent.appendChild(makeSpan("</", "op5-spec-rsiveo-xmlbox-tagHeader"));
+					parent.appendChild(makeSpan(xml.nodeName, "op5-spec-rsiveo-xmlbox-tagValue"));
+					parent.appendChild(makeSpan(">", "op5-spec-rsiveo-xmlbox-tagHeader"));
+				} else {
+					var closingSpan = document.createElement("span");
+					closingSpan.innerText = "/>";
+					parent.appendChild(closingSpan);
+				}
+				break;
 
-                        if (expandingNode) {
-                            var ulElement = document.createElement("ul");
-                            for (var i = 0 ; i < xml.childNodes.length ; i++) {
-                                var liElement = document.createElement("li");
-                                this.buildXmlViewNode(liElement, xml.childNodes[i]);
-                                ulElement.appendChild(liElement);
-                            }
+			case 3: // text
+				if( xml.nodeValue.trim() !== "" ) {
+					parent.appendChild(makeSpan("", "op5-spec-rsiveo-xmlbox-expander"));
+					parent.appendChild(makeSpan(xml.nodeValue));
+				}
+				break;
 
-                             ulElement.setAttribute("class", "op5-spec-rsiveo-xmlbox-content");
-                            //parent.appendChild(collapsedTextSpan);
-                            parent.appendChild(ulElement);
+			case 4: // cdata
+				parent.appendChild(makeSpan("", "op5-spec-rsiveo-xmlbox-expander"));
+				parent.appendChild(makeSpan("<![CDATA[", "op5-spec-rsiveo-xmlbox-tagHeader"));
+				parent.appendChild(makeSpan(xml.nodeValue, "op5-spec-rsiveo-xmlbox-cdata"));
+				parent.appendChild(makeSpan("]]>", "op5-spec-rsiveo-xmlbox-tagHeader"));
+				break;
 
-                            parent.appendChild(makeSpan("", "op5-spec-rsiveo-xmlbox-expanderClose"));
-                        }
-                        else {
-                            parent.appendChild(makeSpan(xml.childNodes[0].nodeValue));
-                        }
+			case 8: // comment
+				parent.appendChild(makeSpan("", "op5-spec-rsiveo-xmlbox-expander"));
+				parent.appendChild(makeSpan("<!--" + xml.nodeValue + "-->", "op5-spec-rsiveo-xmlbox-comment"));
+				break;
 
-                        // Closing tag
-                        parent.appendChild(makeSpan("</", "op5-spec-rsiveo-xmlbox-tagHeader"));
-                        parent.appendChild(makeSpan(xml.nodeName, "op5-spec-rsiveo-xmlbox-tagValue"));
-                        parent.appendChild(makeSpan(">", "op5-spec-rsiveo-xmlbox-tagHeader"));
-                    } else {
-                        var closingSpan = document.createElement("span");
-                        closingSpan.innerText = "/>";
-                        parent.appendChild(closingSpan);
-                    }
-                }
-                break;
+			default:
+				var item = document.createElement("span");
+				item.innerText = "" + xml.nodeType + " - " + xml.name;
+				parent.appendChild(item);
+				break;
+		}
 
-            case 3: // text
-                {
-					if( xml.nodeValue.trim() !== "" ) {
-						parent.appendChild(makeSpan("", "op5-spec-rsiveo-xmlbox-expander"));
-						parent.appendChild(makeSpan(xml.nodeValue));
-					}
-                }
-                break;
+		function makeSpan(innerText) {
+			return makeSpan(innerText, undefined);
+		}
 
-            case 4: // cdata
-                {
-                    parent.appendChild(makeSpan("", "op5-spec-rsiveo-xmlbox-expander"));
-                    parent.appendChild(makeSpan("<![CDATA[", "op5-spec-rsiveo-xmlbox-tagHeader"));
-                    parent.appendChild(makeSpan(xml.nodeValue, "op5-spec-rsiveo-xmlbox-cdata"));
-                    parent.appendChild(makeSpan("]]>", "op5-spec-rsiveo-xmlbox-tagHeader"));
-                }
-                break;
+		function makeSpan(innerText, classes) {
+			var span = document.createElement("span");
+			span.innerText = innerText;
 
-            case 8: // comment
-                {
-                    parent.appendChild(makeSpan("", "op5-spec-rsiveo-xmlbox-expander"));
-                    parent.appendChild(makeSpan("<!--" + xml.nodeValue + "-->", "op5-spec-rsiveo-xmlbox-comment"));
-                }
-                break;
+			if (classes != undefined)
+					span.setAttribute("class", classes);
 
-            default:
-                {
-                    var item = document.createElement("span");
-                    item.innerText = "" + xml.nodeType + " - " + xml.name;
-                    parent.appendChild(item);
-                }
-                break;
-        }
-
-        function makeSpan(innerText) {
-            return makeSpan(innerText, undefined);
-        }
-
-        function makeSpan(innerText, classes) {
-            var span = document.createElement("span");
-            span.innerText = innerText;
-
-            if (classes != undefined)
-                span.setAttribute("class", classes);
-
-            return span;
-        }
-	},
+			return span;
+		}
+	}
 	
 }) ;
